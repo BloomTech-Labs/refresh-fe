9a7add607037c00e37337a3ce504e0d3f35d44fb
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-11-07 17:05:17 -0500

    Refactored ClockDisplay with class components as that's what the best tutorial uses (hope that's fine w/ native!).

--- src/components/clock/ClockDisplay.js
+++ src/components/clock/ClockDisplay.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Timer from './Timer';
 import TimerControllers from './TimerControllers';
 
-export default function ClockDisplay() {
+export default class ClockDisplay extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cycle: "Work",
+            workTime: 25,
+            breakTime: 5
+        }
+    }
 
-    return (
-        <div className="clock-display">
-            <h1>Pomodoro Clock</h1>
-            <Timer />
-            <TimerControllers />
-        </div>
-    )
+    render() {
 
+        return (
+            <div className="clock-display">
+                <h1>Pomodoro Clock</h1>
+                <Timer />
+                <TimerControllers />
+            </div>
+        )
+
+    }
 }
