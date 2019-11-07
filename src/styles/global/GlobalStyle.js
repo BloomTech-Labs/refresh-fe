5c808516d4be36aca2e83f6588f6154e89b9112b
Josh Cooter <thejoshcooter@gmail.com>
2019-11-07 13:30:12 -0600

    change font family to sans serif in global

--- src/styles/global/GlobalStyle.js
+++ src/styles/global/GlobalStyle.js
@@ -13,6 +13,8 @@ const GlobalStyle = createGlobalStyle`
     }
     body {
         height:100%;
+        font-family: sans-serif;
+
         h1,h2,h3,h4,h5 {
             font-size: 2rem;
         }
