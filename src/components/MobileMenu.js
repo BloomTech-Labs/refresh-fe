9c026855e6cf769c5d72cef90c8a243bec8d16bc
Josh Cooter <thejoshcooter@gmail.com>
2019-11-07 02:17:22 -0600

    stub out MobileMenu component. Add components directory for Melissa to add atoms to.

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,15 @@
+// IMPORTS
+// react
+import React from 'react';
+
+// COMPONENT
+const MobileMenu = () => {
+    return (
+        <React.Fragment>
+            hello from MobileMenu
+        </React.Fragment>
+    );
+};
+
+// EXPORT
+export default MobileMenu;
