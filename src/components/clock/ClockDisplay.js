903c3f34d99194e6733e78640a696640bdef946b
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-11-07 17:11:41 -0500

    Added helper functions to increment and decrement work/break time and passed to TimerControllers as props.

--- src/components/clock/ClockDisplay.js
+++ src/components/clock/ClockDisplay.js
@@ -12,13 +12,44 @@ export default class ClockDisplay extends Component {
         }
     }
 
+    incrementWorkTime = () => {
+        this.setState({
+            workTime: this.state.workTime + 1
+        })
+    };
+
+    decrementWorkTime = () => {
+        this.setState({
+            workTime: this.state.workTime - 1
+        })
+    };
+
+    incrementBreakTime = () => {
+        this.setState({
+            breakTime: this.state.breakTime + 1
+        })
+    };
+
+    decrementBreakTime = () => {
+        this.setState({
+            breakTime: this.state.breakTime - 1
+        })
+    };
+
     render() {
 
         return (
             <div className="clock-display">
                 <h1>Pomodoro Clock</h1>
                 <Timer />
-                <TimerControllers />
+                <TimerControllers 
+                    workTime={this.state.workTime}
+                    breakTime={this.state.breakTime}
+                    incrementWorkTime={this.incrementWorkTime}
+                    decrementWorkTime={this.decrementWorkTime}
+                    incrementBreakTime={this.incrementBreakTime}
+                    decrementBreakTime={this.decrementBreakTime}
+                />
             </div>
         )
 
