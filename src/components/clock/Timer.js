9a4e27b0bdb2bf39d4313bfd16f472a776bcc91a
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-11-07 18:03:10 -0500

    Added timer function to Timer component. Think there's a bit more logic required before it will work.

--- src/components/clock/Timer.js
+++ src/components/clock/Timer.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Timer () {
-    return (
-        <div className="timer">
-            <span className="count-down" // toggles timerRunning from false/true
-                  onClick={this.timer}>
-                  {this.props.currentTime}
-            </span>
-            <span>{this.props.cycle}</span>
-        </div>
-    )
+export default class Timer extends Component {
+
+    timer = () => {
+        if (this.props.timerRunning === true) {
+            clearInterval(this.props.timerId)
+            this.props.setCurrentTime("25 : 00")
+            this.props.setTimerRunning()
+        } else {
+            this.props.cycle === "Session" ?
+            this.props.startTimer(this.props.workTime) :
+            this.props.startTimer(this.props.breakTime)
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="timer">
+                <span className="count-down" // toggles timerRunning from false/true
+                    onClick={this.timer}>
+                    {this.props.currentTime}
+                </span>
+                <span>{this.props.cycle}</span>
+            </div>
+        )
     
+    }
 }
