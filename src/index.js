6784d612f47e1f9e2b5023f7410873a6a83c5194
MichaelBaynon <baynonmichael@gmail.com>
2019-11-07 14:58:15 -0500

    added all questionare files and routes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 // IMPORTS
 // react
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // router
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // themes and styles
-import Reset from './styles/global/Reset';
-import GlobalStyle from './styles/global/GlobalStyle';
+import Reset from "./styles/global/Reset";
+import GlobalStyle from "./styles/global/GlobalStyle";
 // components
-import App from './App';
+import App from "./App";
 import CreateAccount from "./views/onboarding/CreateAccount";
+// questions/data collection
+import Weight from "./views/questionare/weight";
+import Height from "./views/questionare/height";
+import Water from "./views/questionare/water";
+import Stretches from "./views/questionare/stretches";
+import Sleep from "./views/questionare/sleep";
+import Overwhelmed from "./views/questionare/overwhelmed";
+import Game from "./views/questionare/game";
+import Food from "./views/questionare/food";
+import Exercise from "./views/questionare/exercise";
+import Breaks from "./views/questionare/breaks";
 
 // RENDER
 ReactDOM.render(
-    <Router>
-        <Reset />
-        <GlobalStyle />
-        <Route exact path='/' render={props => <App  {...props} />} />      
-        <Route 
-            exact 
-            path = "/signup" 
-            render ={props => <CreateAccount {...props} />}
-             />
-    </Router>,
-document.getElementById('root'));
-
+  <Router>
+    <Reset />
+    <GlobalStyle />
+    <Route exact path="/" render={props => <App {...props} />} />
+    <Route
+      exact
+      path="/signup"
+      render={props => <CreateAccount {...props} />}
+    />
+    <Route exact path="/weight" render={props => <Weight {...props} />} />
+    <Route exact path="/height" render={props => <Height {...props} />} />
+    <Route exact path="/water" render={props => <Water {...props} />} />
+    <Route exact path="/stretches" render={props => <Stretches {...props} />} />
+    <Route exact path="/sleep" render={props => <Sleep {...props} />} />
+    <Route exact path="/overwhelmed" render={props => <Overwhelmed {...props} />} />
+    <Route exact path="/game" render={props => <Game {...props} />} />
+    <Route exact path="/food" render={props => <Food {...props} />} />
+    <Route exact path="/exercise" render={props => <Exercise {...props} />} />
+    <Route exact path="/breaks" render={props => <Breaks {...props} />} />
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/views/questionare/height.js
@@ -0,0 +1,97 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// images
+
+const Landing = (props) => {
+    const routeToHeight = e => {
+        e.preventDefault();
+        props.history.push("/overwhelmed");
+    };
+    
+    return(
+        <OnBoardWrapper>
+        <OnBoardContainer>
+            
+            <FlexHolder>
+                <Question>height?</Question>
+                <OnboardTxt>Dont worry, this stays between us</OnboardTxt>
+               
+            </FlexHolder>
+            <FlexHolder>
+                <Button onClick = {routeToHeight}>Continue</Button>
+                <ButtonNoColor onClick = {routeToHeight}>I don't feel comfortable answering</ButtonNoColor>
+            </FlexHolder>
+      </OnBoardContainer>
+      </OnBoardWrapper>
+    )
+}
+
+// STYLED COMPONENTS
+//Onboarding Reusable Styles 
+// we abstract out reusable global styles later on -JC
+const OnBoardWrapper = styled.div`
+background-color: #3a3699;
+width: 100vw;
+height:100vh;
+max-height:100vh;
+`
+
+const OnBoardContainer = styled.div`
+display:flex;
+flex-direction:column;
+align-items:flex-start;
+font-family: 'Catamaran', sans-serif;
+margin:auto;
+line-height:1.5;
+background-color: #3a3699;
+color: #7f7cca;
+`
+const Question = styled.h1`
+font-weight: bold;
+font-size: 5rem;
+line-height: 82px;
+letter-spacing: 3.5px;
+color: #FFFFFF;
+margin-top: 140px;
+`
+
+const OnboardTxt = styled.p`
+font-size: 2rem;
+line-height: 33px;
+letter-spacing: 0.035em;
+color: #CCC9FF;
+`
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 0.5rem;
+  padding: 1.5rem 0.8rem;
+  width:100%;
+  text-align:center;
+  margin: auto;
+  background: #6487ff;
+  color: white;
+  font-size:1.6rem;
+}
+`
+const ButtonNoColor = styled.a`
+    margin:auto;
+    font-size:1.6rem;
+`
+
+const FlexHolder = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin:auto;
+align-items: flex-start;
+width:100%;
+padding: 2.5rem 4rem;
+`
+
+
+
+
+export default Landing; 
