baa2919f1f4a951dd70fe175d27946bd591aea23
Josh Cooter <thejoshcooter@gmail.com>
2019-11-07 02:41:08 -0600

    stub out base component and styles

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,15 +1,53 @@
 // IMPORTS
 // react
-import React from 'react';
+import React, { useState } from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../styles/global/Mixins'
 
 // COMPONENT
 const MobileMenu = () => {
+    // state hooks
+    const [menu, setMenu] = useState({
+        status: 'closed'
+    });
+
+    // use effect
+    // todo
+
+   if (menu.status === 'open') {
+
+   } else {
     return (
-        <React.Fragment>
-            hello from MobileMenu
-        </React.Fragment>
+        <Wrapper>
+            <Container>
+                <PlaceholderButton>Hi</PlaceholderButton>
+            </Container>
+        </Wrapper>
     );
+   }
+
 };
+
+// STYLED COMPONENTS
+const Wrapper = styled.div`
+    width: 100vw;
+`
+
+const Container = styled.div`
+    margin: 0 auto;
+    width: 100vw;
+    height: 5rem;
+    ${flex.flexRowNoWrapAround}
+    ${test.redBg}
+`
+
+const PlaceholderButton = styled.button`
+    width: 3rem;
+    height: 3rem;
+    margin: 2rem auto 2rem 2rem;
+`
 
 // EXPORT
 export default MobileMenu;
