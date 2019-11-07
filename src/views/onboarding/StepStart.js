1c43bd0526b97c761f3f94f437f7106a83523bed
TinaDelli <cecurrier92@gmail.com>
2019-11-07 16:22:03 -0500

    moved styles over to info intro

--- src/views/onboarding/StepStart.js
+++ src/views/onboarding/StepStart.js
@@ -3,12 +3,11 @@
 import React, {useState} from 'react';
 import {useContext} from 'react';
 import { userContext } from '../../contexts/UserContext';
-// styled components
-import styled from 'styled-components';
+
 // images
 
 const StepStart =(props) =>{
-const context = useContext(UserContext);
+const context = useContext(userContext);
 
 const [form, setForm] = useState([]);
 const [currentStep, setCurrentStep] = useState(0);
@@ -42,62 +41,6 @@ const onSubmit = form => {
     )
 }
 
-// STYLED COMPONENTS
-//Onboarding Reusable Styles 
-
-const OnBoardWrapper = styled.div`
-background-color: #3a3699;
-width: 100vw;
-height:100vh;
-max-height:100vh;
-`
-
-const OnBoardContainer = styled.div`
-display:flex;
-flex-direction:column;
-align-items:flex-start;
-font-family: 'Catamaran', sans-serif;
-margin:auto;
-line-height:1.5;
-background-color: #3a3699;
-color: #7f7cca;
-`
-const Refresh = styled.h1`
-font-weight: bold;
-font-size: 5rem;
-line-height: 82px;
-letter-spacing: 3.5px;
-color: #FFFFFF;
-`
-
-const OnboardTxt = styled.p`
-font-size: 2rem;
-line-height: 33px;
-letter-spacing: 0.035em;
-color: #CCC9FF;
-`
-const Button = styled.a`
-  display: inline-block;
-  border-radius: 0.5rem;
-  padding: 1.5rem 0.8rem;
-  width:100%;
-  text-align:center;
-  margin: auto;
-  background: #6487ff;
-  color: white;
-  font-size:1.6rem;
-}
-`
-
-const FlexHolder = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-margin:auto;
-align-items: flex-start;
-width:100%;
-padding: 2.5rem 4rem;
-`
 
 
 export default StepStart;
