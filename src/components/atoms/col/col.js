c86ccc09733f448c9d5b934f6268883db81a8821
melissa-murphy <melissamurphy.ca@gmail.com>
2019-11-08 23:36:59 -0800

    Col done

---
+++ src/components/atoms/col/col.js
@@ -0,0 +1,59 @@
+import React from "react";
+import StyledAtom from "../../../styles/global/components.css";
+
+const Col = ({ ...props }) => {
+  const {
+    display,
+    width,
+    height,
+    border,
+    borderLeft,
+    borderRight,
+    borderBottom,
+    borderRadiusBottomLeft,
+    borderRadiusTopLeft,
+    borderRadiusTopRight,
+    borderRadiusBottomRight,
+    boxShadow,
+    backgroundColor,
+    color,
+    fontSize,
+    margin,
+    padding,
+    justifyContent,
+    flex,
+    flexFlow,
+    textAlign
+  } = props;
+
+  return (
+    <StyledAtom
+      className="baseCol"
+      display={display}
+      width={width}
+      height={height}
+      border={border}
+      borderLeft={borderLeft}
+      borderRight={borderRight}
+      borderBottom={borderBottom}
+      borderRadiusBottomLeft={borderRadiusBottomLeft}
+      borderRadiusTopLeft={borderRadiusTopLeft}
+      borderRadiusTopRight={borderRadiusTopRight}
+      borderRadiusBottomRight={borderRadiusBottomRight}
+      boxShadow={boxShadow}
+      backgroundColor={backgroundColor}
+      color={color}
+      fontSize={fontSize}
+      margin={margin}
+      padding={padding}
+      justifyContent={justifyContent}
+      flex={flex}
+      flexFlow={flexFlow}
+      textAlign={textAlign}
+    >
+      {props.children}
+    </StyledAtom>
+  );
+};
+
+export default Col;
--- src/views/componentTesting/componentTesting.js
+++ src/views/componentTesting/componentTesting.js
@@ -6,13 +6,14 @@ import Input from "../../components/atoms/input/input";
 import Link from "../../components/atoms/link/link";
 import Row from "../../components/atoms/row/row";
 import Card from "../../components/atoms/card/card";
+import Col from "../../components/atoms/col/col";
 
 const google = "https://www.google.com/";
 const Atoms = props => {
   return (
     <div>
       <Button width={50}>Imma button yo</Button>
-      <Badge width={15} text={'I am a badge'} />
+      <Badge width={15} text={"I am a badge"} />
       <Input
         // width={350}
         margin={2}
@@ -25,16 +26,27 @@ const Atoms = props => {
       <Row backgroundColor={Colors.red}>
         <p>I'm a row</p>
       </Row>
-      <Row>
-        <Card>
+      <Row backgroundColor={"white"}>
+        <Card textAlign={"center"}>
           <p>Hi! I am a card inside of a row!</p>
         </Card>
-        <Card>
+        <Card textAlign={"center"}>
           <p>Me too!</p>
         </Card>
-        <Card>
+        <Card textAlign={"center"}>
           <p>Me three!</p>
         </Card>
+      </Row>
+      <Row>
+        <Col>
+          <p>Hi! I am a column inside of a row!</p>
+        </Col>
+        <Col>
+          <p>Me too!</p>
+        </Col>
+        <Col>
+          <p>Me three!</p>
+        </Col>
       </Row>
     </div>
   );
