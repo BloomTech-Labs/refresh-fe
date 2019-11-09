40368f14a78351846d9683553cde1982c4c6a2db
Josh Cooter <thejoshcooter@gmail.com>
2019-11-09 05:05:51 -0600

    stub out dashboard component

---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Dashboard = () => {
+    return (
+        <div>
+            hi from dashboard
+        </div>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default Dashboard;
