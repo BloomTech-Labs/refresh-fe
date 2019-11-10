837edee40de17ec307f4a364475994970822c893
Josh Cooter <thejoshcooter@gmail.com>
2019-11-10 07:18:03 -0600

    add Catamaran font to global

--- src/styles/global/GlobalStyle.js
+++ src/styles/global/GlobalStyle.js
@@ -13,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
     }
     body {
         height:100%;
-        font-family: sans-serif;
+        font-family: 'Catamaran', sans-serif;
         font-size: 1.25rem;
         background-color: #3a3699;
 
