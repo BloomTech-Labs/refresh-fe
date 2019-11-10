b566dc8652e52c7ec8f0043eeb79902fb6e41c29
Josh Cooter <thejoshcooter@gmail.com>
2019-11-10 07:13:33 -0600

    structure out inner cards. add styling to copy.

--- src/views/dashboard/MissionCard.js
+++ src/views/dashboard/MissionCard.js
@@ -12,6 +12,7 @@ const MissionCard = props => {
     
     return (
         <Container color={props.color}>
+            <i class="fas fa-tint"></i>
             <p>{props.description}</p>
             <span>{props.points} Points</span>
         </Container>
@@ -26,6 +27,28 @@ const Container = styled.div`
     border-radius: 3px;
     margin: 1rem 0.5rem;
     background-color: ${props => props.color};
+    padding: 1rem;
+    ${flex.flexCol}
+
+        // this i needs to change later on when i work out
+        // the gauge functionality for the cards -JC
+    
+        i {
+            font-size: 3rem;
+            margin: 2rem 0;
+        }
+    
+        p {
+            font-size: 1.4rem;
+            letter-spacing: 0.025rem;
+            align-self: flex-start;
+        }
+    
+        span {
+            font-weight: bold;
+            align-self: flex-start;
+            margin-top: auto;
+        }
 `
 
 // EXPORT
