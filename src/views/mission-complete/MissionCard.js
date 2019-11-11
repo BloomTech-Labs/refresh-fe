485cc42aa7bd3b7ea9088275357e0de75d8a43f2
Josh Cooter <thejoshcooter@gmail.com>
2019-11-10 23:20:41 -0600

    stub out mission card component

---
+++ src/views/mission-complete/MissionCard.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const MissionCard = () => {
+    return (
+        <div>
+            mission card
+        </div>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default MissionCard;
