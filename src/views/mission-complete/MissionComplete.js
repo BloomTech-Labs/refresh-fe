15be49d5dbfe6ee91917e509cf62887cd6c7efeb
Josh Cooter <thejoshcooter@gmail.com>
2019-11-10 23:07:32 -0600

    stub out mission complete component

---
+++ src/views/mission-complete/MissionComplete.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const MissionComplete = () => {
+    return (
+        <div>
+            hi from missioncomplete
+        </div>
+    );
+};
+
+// STYLED COMPONENTS
+
+
+// EXPORT
+export default MissionComplete;
