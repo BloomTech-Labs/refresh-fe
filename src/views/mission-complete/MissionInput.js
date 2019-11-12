d9cc9bbf17064a85229df53cc907888807449d6c
Josh Cooter <thejoshcooter@gmail.com>
2019-11-12 11:48:32 -0600

    stub out mission input component

---
+++ src/views/mission-complete/MissionInput.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react 
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const MissionInput = () => {
+    return (
+        <div>
+            hi from mission input
+        </div>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default MissionInput;
