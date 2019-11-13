2b0a5b1ff7960ddea03f2c58f8c9dd11a10d0667
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 14:36:45 -0600

    stub out sandbox component

---
+++ src/views/sandbox/Sandbox.js
@@ -0,0 +1,26 @@
+// WELCOME TO THE SANDBOX
+// There are a lot of people testing things in random places and adding random routes.
+// Instead of doing that please use this sandbox for anything that you need to test.
+// -JC
+
+// IMPORTS
+// react 
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Sandbox = () => {
+    return (
+        <>
+            welcome to the sandbox
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
