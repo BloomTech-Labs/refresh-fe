3bfff330334d00b9bc60b18fa0d40e690b7558da
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 14:38:56 -0600

    add notes to sandbox.js

--- src/views/sandbox/Sandbox.js
+++ src/views/sandbox/Sandbox.js
@@ -1,6 +1,8 @@
 // WELCOME TO THE SANDBOX
 // There are a lot of people testing things in random places and adding random routes.
 // Instead of doing that please use this sandbox for anything that you need to test.
+// If I need to come back and add default page styling so that you guys can see things
+// in line with what you would see on a normal page, just let me know and I will fix
 // -JC
 
 // IMPORTS
@@ -24,3 +26,4 @@ const Sandbox = () => {
 // todo
 
 // EXPORT
+export default Sandbox;
