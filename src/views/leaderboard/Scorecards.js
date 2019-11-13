dfd13817e52cc8707ceded8cbfa0d75dfcc69ba5
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 15:59:20 -0600

    stub out scoreboard component

---
+++ src/views/leaderboard/Scorecards.js
@@ -0,0 +1,23 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Scoreboard = () => {
+    return (
+        <>
+            hi from scoreboard
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// TODO
+
+
+// EXPORT
+export default Scoreboard;
