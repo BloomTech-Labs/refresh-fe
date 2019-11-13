c759efab6b403241400d451de23e3b7d177ed660
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 14:56:38 -0600

    add dummy data

--- src/views/leaderboard/Leaderboard.js
+++ src/views/leaderboard/Leaderboard.js
@@ -6,6 +6,45 @@ import styled from 'styled-components';
 // helpers
 import { test, flex } from '../../styles/global/Mixins';
 
+// DUMMY DATA
+const data = [
+    {
+        fname: 'Bob',
+        lname: 'Bobberton',
+        cohort: 'Web22',
+        sectionLead: 'Bryan',
+        points: 100
+    },
+    {
+        fname: 'Tom',
+        lname: 'Tommerton',
+        cohort: 'Web26',
+        sectionLead: 'Hannah',
+        points: 120
+    },
+    {
+        fname: 'Jim',
+        lname: 'Jimbob',
+        cohort: 'UX5',
+        sectionLead: 'Jessica',
+        points: 140
+    },
+    {
+        fname: 'John',
+        lname: 'Doe',
+        cohort: 'DS3',
+        sectionLead: 'Hank',
+        points: 160
+    },
+    {
+        fname: 'Susan',
+        lname: 'Susan',
+        cohort: 'UX6',
+        sectionLead: 'George',
+        points: 180
+    }
+];
+
 // COMPONENT
 const Leaderboard = () => {
     return (
