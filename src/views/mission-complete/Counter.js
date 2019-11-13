b22f36f8404111b8d3db337aa987d5392ca481bb
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 01:59:01 -0600

    add styled input buttons

--- src/views/mission-complete/Counter.js
+++ src/views/mission-complete/Counter.js
@@ -9,14 +9,48 @@ import { test, flex } from '../../styles/global/Mixins';
 // COMPONENT
 const Counter = () => {
     return (
-        <React.Fragment>
-            hi from counter
-        </React.Fragment>
+        <CounterWrapper>
+            <button>-</button>
+            <Display>7</Display>
+            <button>+</button>
+        </CounterWrapper>
     );
 };
 
 // STYLED COMPONENTS
-// todo
+const CounterWrapper = styled.div`
+    width: 100%;
+    margin: 1rem;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    ${test.redBg}
+
+        button {
+            width: 40px;
+            height: 40px;
+            border-radius: 5px;
+            border: none;
+            background-color: rgba(0, 0, 0, 0.5);
+            color: #FFF;
+            font-size: 2rem;
+        }
+
+        div {
+            margin: 1rem;
+        }
+`
+
+const Display = styled.div`
+        width: 50px;
+        height: 50px;
+        border-radius: 5px;
+        border: none;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: #FFF;
+        font-size: 2rem;
+`
 
 // EXPORT
 export default Counter;
