1af166f8d88394f18589ab57189b658c5575f14f
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 01:44:04 -0600

    stub out counter

---
+++ src/views/mission-complete/Counter.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react 
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers 
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Counter = () => {
+    return (
+        <React.Fragment>
+            hi from counter
+        </React.Fragment>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default Counter;
