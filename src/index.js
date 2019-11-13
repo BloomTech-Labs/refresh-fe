a616fe1afef012a7923797a614c204c87edc859a
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 13:49:24 -0600

    fix deleted router issues

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // router
-//import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // themes and styles
-//import Reset from "./styles/global/Reset";
-//import GlobalStyle from "./styles/global/GlobalStyle";
+import Reset from "./styles/global/Reset";
+import GlobalStyle from "./styles/global/GlobalStyle";
 // components
 import App from './App';
-//import CreateAccount from './views/onboarding/CreateAccount';
 
 
 
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
