e73a5aff9f44d73858893ed586551f490c4f071c
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 13:47:56 -0600

    readd deleted mixins file

---
+++ src/styles/global/Mixins.js
@@ -0,0 +1,51 @@
+// GLOBAL MIXINS
+
+
+// JOSH MIXINS
+export const test = {
+    redOutline: `border: 2px solid red;`,
+    blueOutline: `border: 2px solid blue;`,
+    greenOutline: `border: 2px solid green;`,
+    redBg: `background-color: red;`,
+    blueBg: `background-color: blue;`,
+    greenBg: `background-color: green;`
+};
+
+export const flex = {
+    flexRowNoWrapAround: `
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+        align-items: center;
+    `,
+    flexRowNoWrapBetween: `
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        align-items: center;
+    `,
+    flexRowWrapAround: `
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-around;
+        align-items: center;
+    `,
+    flexRowWrapBetween: `
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+        align-items: center;
+    `,
+    flexCol: `
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+    `,
+    flexColWrap: `
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
+    `
+};
