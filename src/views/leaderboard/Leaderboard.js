428c9d3ec3db43e5db639c219c5d2776d0e2f8ef
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 14:31:19 -0600

    stub out leaderboard component

---
+++ src/views/leaderboard/Leaderboard.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Leaderboard = () => {
+    return (
+        <>
+            hi from leaderboard
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default Leaderboard;
