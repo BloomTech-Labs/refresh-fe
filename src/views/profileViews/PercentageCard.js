940ab1e55a083acf9a8b62c4a086a9304e667bd2
gina-christians <christiansgina@gmail.com>
2019-11-14 11:41:34 -0700

    percentage card code base component styles

--- src/views/profileViews/PercentageCard.js
+++ src/views/profileViews/PercentageCard.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Rectangle50 = styled.div`
+position: absolute;
+width: 134px;
+height: 207px;
+left: 221px;
+top: 233px;
 
+/* primary / disabled */
+
+background: #CCC9FF;
+/* mobile / dashboard dropshadow */
+
+box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
+border-radius: 2px;
+`
+
+const ProfileText = styled.h5`
+position: absolute;
+width: 46px;
+height: 9px;
+left: 265px;
+top: 232px;
+
+font-family: 'Catamaran', sans-serif;
+font-style: normal;
+font-weight: normal;
+font-size: 16px;
+line-height: 44%;
+/* or 7px */
+
+letter-spacing: 0.02em;
+
+/* primary / purple */
+
+color: #6762E3;
+`
 
 const PercentageCard = () => {
     return (
         <>
-        null;
+        <Rectangle50/>
+        <PercentageCard>%</PercentageCard>
         </>
     );
 };
