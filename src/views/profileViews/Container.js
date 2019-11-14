577e122a62e57aa5a726736acb2c48dcacecaadf
gina-christians <christiansgina@gmail.com>
2019-11-13 18:38:59 -0700

    base component for Header.js

--- src/views/profileViews/Container.js
+++ src/views/profileViews/Container.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
+//import ActivityCard from './ActivityCard';
+//import PercentageCard from '../profileViews/PercentageCard';
+//import HeaderCard from '../profileViews/HeaderCard';
+//import ContactCard from '../profileViews/ContactCard';
+//import WeeklySurvey from '../profileViews/WeeklySurvey';
+////import WaterCard from '../profileViews/WaterCard';
+//import Badges from '../profileViews/Badges';
+//import YourReminders from '../profileViews/YourReminders';
+//import ProfileAv from '../profileViews/ProfileAv';
 
 
 
---
+++ src/views/profileViews/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+
+const HeaderCard = () => {
+    return (
+        <>
+        null;
+        </>
+    );
+};
+
+export default HeaderCard;
