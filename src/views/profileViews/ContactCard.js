a47e17b3e2cf87da53b5920afa63344350a8583d
gina-christians <christiansgina@gmail.com>
2019-11-13 18:43:38 -0700

    basec component contactcard.js

---
+++ src/views/profileViews/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const ContactCard = () => {
+    return (
+        <>
+        null;
+        </>
+    );
+};
+
+export default ContactCard;
