6b7793d2f43f301a138ac8d3aa09bcc69ce4d07b
Josh Cooter <thejoshcooter@gmail.com>
2019-11-13 21:28:16 -0600

    add structural styled components

--- src/views/mission-complete/Congrats.js
+++ src/views/mission-complete/Congrats.js
@@ -10,13 +10,69 @@ import { test, flex } from '../../styles/global/Mixins';
 const Congrats = () => {
     return (
         <>
-            hi from congrats
+            <CongratsScreen>
+                <DrawerAccent />
+                <CloseButton />
+            </CongratsScreen>
         </>
     );
 };
 
 // STYLED COMPONENTS
-// TODO
+const CongratsScreen = styled.div`
+    &.open {
+        width: 100vw;
+        height: 35vh;
+        margin: 0 auto;
+        border-radius: 5px;
+        position: fixed;
+        bottom: 0;
+        background-color: #6762E3;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        transition: 0.5s;
+    }
+
+    &.closed {
+        width: 100vw;
+        height: 40vh;
+        margin: 0 auto;
+        border-radius: 5px;
+        position: fixed;
+        bottom: -50vh;
+        background-color: #6762E3;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        transition: 0.5s;
+    }
+
+        button {
+            align-self: flex-end;
+            margin-bottom: auto;
+        }
+`
+
+const DrawerAccent = styled.div`
+    width: 40px;
+    height: 3px;
+    border-radius: 50px;
+    background-color: #FFF;
+    opacity: 0.5;
+    margin: 0.2rem;
+`
+
+const CloseButton = styled.button`
+    width: 25px;
+    height: 25px;
+    border-radius: 150px;
+    border: none;
+    margin: 1rem;
+    opacity: 0.5;
+`
 
 
 // EXPORT
