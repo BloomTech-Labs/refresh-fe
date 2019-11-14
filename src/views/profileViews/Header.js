d30121063aa421516974dea6ee10c88d306dfabd
gina-christians <christiansgina@gmail.com>
2019-11-14 11:27:40 -0700

    added code back in for contact and header js files

--- src/views/profileViews/Header.js
+++ src/views/profileViews/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const YourProfile = styled.h2`
+position: absolute;
+width: 240px;
+height: 56px;
+left: 30px;
+top: 136px;
+
+
+font-family: 'Catamaran', sans-serif;
+font-style: normal;
+font-weight: bold;
+font-size: 35px;
+line-height: 44%;
+/* or 15px */
+
+letter-spacing: 3.5px;
+
+/* primary / white */
+
+color: #FFFFFF;
+
+`
+
 
 
 
 const HeaderCard = () => {
     return (
         <>
-        null;
+         <YourProfile>Your Profile</YourProfile>
         </>
     );
 };
