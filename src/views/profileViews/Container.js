9a952576c7b8f244787bc0692f2f6efe689f3a59
gina-christians <christiansgina@gmail.com>
2019-11-13 18:28:20 -0700

    create base component for container.js

---
+++ src/views/profileViews/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Container = () => {
+    return (
+        <>
+            null;
+        </>
+    );
+};
+
+export default Container;
