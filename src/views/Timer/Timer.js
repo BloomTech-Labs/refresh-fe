0153347f4becec817b90ee94e1cf58f2de8e009c
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-11-14 19:09:23 -0500

    Had to create another Timer.js because of a very odd error message. It's empty. TimerOne.js took its place. Found a button and confirmed linking works. Adding to other components now.

---
+++ src/views/Timer/Timer.js
@@ -0,0 +1,4 @@
+// This component is not in use. I deleted it and had to add it
+// back due to an error message that wouldn't go away. 
+// TimerOne was created to replace as I failed to debug and
+// had to start from scrach.
