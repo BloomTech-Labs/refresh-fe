72b629758d9573907e712225851fbd1ba6bd2562
Josh Cooter <thejoshcooter@gmail.com>
2019-11-15 13:49:30 -0600

    stub out mission stats component

---
+++ src/views/mission-stats/MissionStats.js
@@ -0,0 +1,23 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const MissionStats = () => {
+    return (
+        <>
+            hi from mission stats
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// TODO
+
+
+// EXPORT
+export default MissionStats;
