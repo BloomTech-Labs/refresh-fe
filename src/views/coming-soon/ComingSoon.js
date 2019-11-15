72a869aa5f2b86ead71493be5ba7fabebd1e045a
Josh Cooter <thejoshcooter@gmail.com>
2019-11-15 12:58:27 -0600

    setup structural styled components

--- src/views/coming-soon/ComingSoon.js
+++ src/views/coming-soon/ComingSoon.js
@@ -16,7 +16,30 @@ const ComingSoon = () => {
 };
 
 // STYLED COMPONENTS
-// TODO
+const SoonView = styled.div`
+    width: 100vw;
+    height: 100vh;
+    max-height: 100vh;
+    padding-top: 10rem;
+    background-color: #3a3699;
+`
+
+const SoonWrapper = styled.div`
+    width: 100%;
+`
+
+const SoonContainer = styled.div`
+    width: 90%;
+    height: 80vh;
+    margin: 0 auto;
+
+        .mission-message {
+            color: #CCC9FF;
+            font-size: 2.5rem;
+            letter-spacing: 0.25rem;
+            margin: 3rem 0;
+        }
+`
 
 
 // EXPORT
