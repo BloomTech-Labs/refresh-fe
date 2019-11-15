774fd3959f5aab558888f30b34924d2c921b700b
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-11-14 19:32:26 -0500

    Removed Timer code from App (tried to test in Sandbox but it didn't work and I have no idea why). Also deleted other unnecessary stuff.

--- src/App.js
+++ src/App.js
@@ -14,14 +14,6 @@ import StepStart from "./views/onboarding/steps/StepStart";
 import Login from "./views/onboarding/Login";
 import Leaderboard from './views/leaderboard/Leaderboard';
 import Sandbox from './views/sandbox/Sandbox';
-import TimerOne from './views/Timer/TimerOne';
-import TimerTwo from './views/Timer/TimerTwo';
-import TimerThree from './views/Timer/TimerThree';
-import TimerFour from './views/Timer/TimerFour';
-import TimerShortBreakOne from './views/Timer/TimerShortBreakOne';
-import TimerShortBreakTwo from './views/Timer/TimerShortBreakTwo';
-import TimerShortBreakThree from './views/Timer/TimerShortBreakThree';
-import TimerLongBreak from './views/Timer/TimerLongBreak';
 
 // COMPONENT
 const App = props => {
@@ -38,14 +30,6 @@ const App = props => {
       <Route path="/login" component={Login} />
       <Route path="/atoms" component={Atoms} />
       <Route path='/sandbox' component={Sandbox} />
-      <Route path='/timer-1' component={TimerOne} /> */
-      <Route path='/timer-2' component={TimerTwo} />
-      <Route path='/timer-3' component={TimerThree} />
-      <Route path='/timer-4' component={TimerFour} />
-      <Route exact path='/break-1' component={TimerShortBreakOne} />
-      <Route path='/break-2' component={TimerShortBreakTwo} />
-      <Route path='/break-3' component={TimerShortBreakThree} />
-      <Route path='/break-4' component={TimerLongBreak} />
     </>
     
   );
