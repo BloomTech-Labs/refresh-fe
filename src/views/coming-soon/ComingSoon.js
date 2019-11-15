0a90821f0c08770a5adfe2d93db54b569c74c5df
Josh Cooter <thejoshcooter@gmail.com>
2019-11-15 12:57:37 -0600

    stub out coming soon component

---
+++ src/views/coming-soon/ComingSoon.js
@@ -0,0 +1,23 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const ComingSoon = () => {
+    return (
+        <>
+            hi from coming soon
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// TODO
+
+
+// EXPORT
+export default ComingSoon;
