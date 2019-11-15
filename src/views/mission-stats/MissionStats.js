ea7b95a96a97a4b0bb18c404a94f1dc7e20fc735
Josh Cooter <thejoshcooter@gmail.com>
2019-11-15 14:02:58 -0600

    stub out structural components

--- src/views/mission-stats/MissionStats.js
+++ src/views/mission-stats/MissionStats.js
@@ -16,8 +16,28 @@ const MissionStats = () => {
 };
 
 // STYLED COMPONENTS
-// TODO
+const StatsView = styled.div`
+    width: 100vw;
+    height: 100vh;
+    max-height: 100vh;
+    padding-top: 10rem;
+    background-color: #3a3699;
+`
 
+const StatsWrapper = styled.div`
+    width: 100%;
+`
+
+const StatsContainer = styled.div`
+    width: 90%;
+    height: 80vh;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+
+`
 
 // EXPORT
 export default MissionStats;
