8b43abf869612446086449c3c0cc11a9c9825e10
melissa-murphy <melissamurphy.ca@gmail.com>
2019-11-17 14:41:17 -0800

    gauge updates

--- src/components/molecules/gauge/gauge.js
+++ src/components/molecules/gauge/gauge.js
@@ -6,59 +6,65 @@ import styled from "styled-components";
 // Atoms
 import Icon from "../../atoms/icon/icon";
 // SVG Images
-import step0 from "../../../images/gauge";
-import step1 from "../../../images/gauge/gaugeSteps/step1.svg";
-import step2 from "../../../images/gauge/gaugeSteps/step2.svg";
-import step3 from "../../../images/gauge/gaugeSteps/step3.svg";
-import step4 from "../../../images/gauge/gaugeSteps/step4.svg";
-import step5 from "../../../images/gauge/gaugeSteps/step5.svg";
-import step6 from "../../../images/gauge/gaugeSteps/step6.svg";
-import step7 from "../../../images/gauge/gaugeSteps/step7.svg";
-import step8 from "../../../images/gauge/gaugeSteps/step8.svg";
+import water0 from "../../../images/gauge/waterGauge/water0.svg";
+import water1 from "../../../images/gauge/waterGauge/water1.svg";
+import water2 from "../../../images/gauge/waterGauge/water2.svg";
+import water3 from "../../../images/gauge/waterGauge/water3.svg";
+import water4 from "../../../images/gauge/waterGauge/water4.svg";
+import water5 from "../../../images/gauge/waterGauge/water5.svg";
+import water6 from "../../../images/gauge/waterGauge/water6.svg";
+import water7 from "../../../images/gauge/waterGauge/water7.svg";
+import waterComplete from "../../../images/gauge/waterGauge/water_complete.svg";
 
-let waterGaugeImages = [
-  { src: step0, altText: "0 glasses of water", currentWater: 0 },
-  { src: step1, altText: "1 glasses of water", currentWater: 1 },
-  { src: step2, altText: "2 glasses of water", currentWater: 2 },
-  { src: step3, altText: "3 glasses of water", currentWater: 3 },
-  { src: step4, altText: "4 glasses of water", currentWater: 4 },
-  { src: step5, altText: "5 glasses of water", currentWater: 5 },
-  { src: step6, altText: "6 glasses of water", currentWater: 6 },
-  { src: step7, altText: "7 glasses of water", currentWater: 7 },
-  { src: step8, altText: "8 glasses of water", currentWater: 8 }
-];
+let waterGaugeImages = {
+  water0: [{ src: water0, altText: "0 glasses of water", currentWater: 0 }],
+  water1: [{ src: water1, altText: "1 glasses of water", currentWater: 1 }],
+  water2: [{ src: water2, altText: "2 glasses of water", currentWater: 2 }],
+  water3: [{ src: water3, altText: "3 glasses of water", currentWater: 3 }],
+  water4: [{ src: water4, altText: "4 glasses of water", currentWater: 4 }],
+  water5: [{ src: water5, altText: "5 glasses of water", currentWater: 5 }],
+  water6: [{ src: water6, altText: "6 glasses of water", currentWater: 6 }],
+  water7: [{ src: water7, altText: "7 glasses of water", currentWater: 7 }],
+  waterComplete: [
+    { src: waterComplete, altText: "8 glasses of water", currentWater: 8 }
+  ]
+};
 
 const Gauge = props => {
+  const[gauge, setGauge] = useState(waterGaugeImages)
   const [user, setUser] = useState({
     hydrationStats: 4
   });
+  console.log(user);
+  
   const [waterGauge, setWaterGauge] = useState(user.hydrationStats);
-  const [currentWaterGauge, setCurrentWaterGauge] = useState(0);
-  let gaugeFill =
-    waterGauge === 0
-      ? setWaterGauge(step0)
-      : waterGauge === 1
-      ? setWaterGauge(step1)
-      : waterGauge === 2
-      ? setWaterGauge(step2)
-      : waterGauge === 3
-      ? setWaterGauge(step3)
-      : waterGauge === 4
-      ? setWaterGauge(step4)
-      : waterGauge === 5
-      ? setWaterGauge(step5)
-      : waterGauge === 6
-      ? setWaterGauge(step6)
-      : waterGauge === 7
-      ? setWaterGauge(step7)
-      : setWaterGauge(step8);
+  // let gaugeFill =
+  //   waterGauge === 0
+  //     ? setWaterGauge(waterGaugeImages.water0)
+  //     : waterGauge === 1
+  //     ? setWaterGauge(waterGaugeImages.water1)
+  //     : waterGauge === 2
+  //     ? setWaterGauge(waterGaugeImages.water2)
+  //     : waterGauge === 3
+  //     ? setWaterGauge(waterGaugeImages.water3)
+  //     : waterGauge === 4
+  //     ? setWaterGauge(waterGaugeImages.water4)
+  //     : waterGauge === 5
+  //     ? setWaterGauge(waterGaugeImages.water5)
+  //     : waterGauge === 6
+  //     ? setWaterGauge(waterGaugeImages.water6)
+  //     : waterGauge === 7
+  //     ? setWaterGauge(waterGaugeImages.water7)
+  //     : setWaterGauge(waterGaugeImages.waterComplete);
+
+      // useEffect axios call for user stats
 
   return (
     <>
       <StyledGauge className="container">
         {/* SVG url should be derived from state */}
         <MobileCardWater>
-          <Icon svg={gaugeFill} alt={"Step 6"} />
+          <Icon svg={waterGaugeImages.water0} alt={"alt"} />
         </MobileCardWater>
       </StyledGauge>
     </>
