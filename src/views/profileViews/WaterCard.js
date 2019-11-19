5261f690560e559545bb1d5b4bb456e6526ea4f5
Josh Cooter <thejoshcooter@gmail.com>
2019-11-19 05:56:42 -0600

    add elements to hydration card

--- src/views/profileViews/WaterCard.js
+++ src/views/profileViews/WaterCard.js
@@ -5,6 +5,8 @@ import React from 'react';
 import styled from 'styled-components';
 // helpers 
 import { test, flex } from '../../styles/global/Mixins';
+// images
+import WaterBottle from '../../images/profile/water_bottle.svg';
 
 
 //This component tracks user water intake
@@ -14,7 +16,10 @@ const WaterCard = (props) => {
   return (
     <>
     <WaterWidget>
-
+      <h3>Hydration<br /> Station</h3>
+      <HydrationImg src={WaterBottle} />
+      <WaterCount><span>0</span>cups of water</WaterCount>
+      <span> of 8</span>
     </WaterWidget>
     </>
   );
@@ -28,7 +33,7 @@ const WaterWidget = styled.div`
     margin: 2rem 0;
     background-color: #CEE2FF;
     box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
-  border-radius: 2px;
+    border-radius: 2px;
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
@@ -36,13 +41,21 @@ const WaterWidget = styled.div`
     color: rgba(255, 255, 255, 0.6);
 
         h3 {
-            font-size: 1.25rem;
-            margin-top: auto;
+            color: #6091D6;
         }
 
         span {
-            margin-bottom: 2rem;
+            color: #1575FF;
         }
+`
+
+const HydrationImg = styled.img`
+    width: 15%;
+    height: auto;
+`
+
+const WaterCount = styled.div`
+        color: #1575FF;
 `
 
 // EXPORT
