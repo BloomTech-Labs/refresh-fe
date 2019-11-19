f2628d3d132158ed0222244fff273008f7e33dc4
Josh Cooter <thejoshcooter@gmail.com>
2019-11-19 05:23:14 -0600

    add basic elements to contact card with some styles

--- src/views/profileViews/ContactCard.js
+++ src/views/profileViews/ContactCard.js
@@ -15,50 +15,75 @@ import { test, flex } from '../../styles/global/Mixins';
 const ContactCard = () => {
     return (
         <>
-         <Rectangle47/>  
-            <ContactInformation> contact
-               information
-            </ContactInformation>  
+          <ContactWidget>
+            <span>...</span>
+            <span>Contact<br /> Information</span>
+          </ContactWidget>
         </>
     );
 };
 
 // STYLED COMPONENTS
-const Rectangle47 = styled.div`
-position: absolute;
-width: 167px;
-height: 60px;
-left: 30px;
-top: 222px;
+const ContactWidget = styled.div`
+    width: 100%;
+    height: 6rem;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    background-color: #D4F7E3;
+    box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
+    border-radius: 2px;
 
-/* primary / disabled green */
+        span:first-child {
+            align-self: flex-end;
+            background: none;
+            border: none;
+            font-size: 2rem;
+            color: #28C96C;
+        }
 
-background: #D4F7E3;
-/* mobile / dashboard dropshadow */
-
-box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
-border-radius: 2px;
+        span:last-child {
+            align-self: flex-start;
+            color: #28C96C;
+        }
 `
-const ContactInformation = styled.h5`
-position: absolute;
-width: 107px;
-height: 28px;
-left: 50px;
-top: 218px;
 
-font-family: 'Catamaran', sans-serif;
-font-style: normal;
-font-weight: normal;
-font-size: 13px;
-line-height: 120%;
-/* or 16px */
+// const Rectangle47 = styled.div`
+// position: absolute;
+// width: 167px;
+// height: 60px;
+// left: 30px;
+// top: 222px;
 
-letter-spacing: 0.02em;
+// /* primary / disabled green */
 
-/* primary / active */
+// background: #D4F7E3;
+// /* mobile / dashboard dropshadow */
 
-color: #28C96C;
-`
+// box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
+// border-radius: 2px;
+// `
+// const ContactInformation = styled.h5`
+// position: absolute;
+// width: 107px;
+// height: 28px;
+// left: 50px;
+// top: 218px;
+
+// font-family: 'Catamaran', sans-serif;
+// font-style: normal;
+// font-weight: normal;
+// font-size: 13px;
+// line-height: 120%;
+// /* or 16px */
+
+// letter-spacing: 0.02em;
+
+// /* primary / active */
+
+// color: #28C96C;
+// `
 
 // EXPORT
 export default ContactCard;
