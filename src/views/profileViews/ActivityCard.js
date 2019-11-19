f6f81f6bb794f319563c0acbab505e3e9f3b7a5b
Josh Cooter <thejoshcooter@gmail.com>
2019-11-19 07:15:15 -0600

    stub out activity widdget main component

--- src/views/profileViews/ActivityCard.js
+++ src/views/profileViews/ActivityCard.js
@@ -4,7 +4,7 @@ import React from 'react';
 // styled components
 import styled from 'styled-components';
 // helpers 
-import {test, flex} from '../../styles/global/Mixins/';
+import {test, flex} from '../../styles/global/Mixins';
 
 //This Component helps the user track their weekly activity
 
@@ -21,7 +21,28 @@ const ActivityCard = () => {
 }
 
 // STYLED COMPONENTS
-// TODO
+const ActivityWidget = styled.div`
+    width: 90%;
+    height: 15rem;
+    margin: 2rem 0;
+    background-color: #6762E3;
+    box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
+    border-radius: 2px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.6);
+
+        h3 {
+            font-size: 1.25rem;
+            margin-top: auto;
+        }
+
+        span {
+            margin-bottom: 2rem;
+        }
+`
 
 
 // EXPORT
