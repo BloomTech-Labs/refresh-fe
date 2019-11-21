b1db5d77f55fa3969086c8e7877dfaf61d67807c
Josh Cooter <thejoshcooter@gmail.com>
2019-11-21 09:57:30 -0600

    stub out dummydata.js for contexts

---
+++ src/contexts/DummyData.js
@@ -0,0 +1,20 @@
+// temporary dummy data to be used for global contexts
+// just until backend matches FE payload structures -JC
+
+// USER MISSION DUMMY DATA
+// shape of data example
+// {
+//     "id": 12,
+//     "vertical": "sleep",
+//     "description": "more hours of sleep",
+//     "question": "How many hours did you sleep for?",
+//     "point_value": 20,
+//     "point_current": 4,***
+//     "goal": 6,
+//     "dotw": null,
+//     "start_date": null,
+//     "ending_date": null,
+//     "daily_reminders": null,
+//     "user_id": null,
+//     "answer_date": "2019-11-20T21:58:25.340Z"
+//   }
