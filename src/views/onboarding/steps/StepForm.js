abd9fc921ce059c4036f1bfcfecbe06b77153281
TinaDelli <cecurrier92@gmail.com>
2019-12-06 21:19:17 -0500

    switched history push to redirect

--- src/views/onboarding/steps/StepForm.js
+++ src/views/onboarding/steps/StepForm.js
@@ -1,6 +1,7 @@
 // IMPORTS
 //react
 import React, { useState, useEffect, useContext } from "react";
+import { Redirect } from "react-router-dom";
 //context
 import { UserContext } from "../../../contexts/UserContext";
 //axios with auth
@@ -23,6 +24,7 @@ const StepForm = props => {
   const [questions, setQuestions] = useState([]);
   const [currentStep, setCurrentStep] = useState(0);
   const [answer, setAnswer] = useState();
+  const [done, setDone] = useState(false);
 
   //Get Questions on Mount
   useEffect(() => {
@@ -48,7 +50,7 @@ const StepForm = props => {
       console.log("here", questions[currentStep]);
       postAnswer({ answer, question_id: questions[currentStep].id });
       setQuestions([]);
-      props.history.push("/dashboard");
+      setDone(true);
     } else {
       const defaultAnswer = answer ? answer : "Chose not to answer";
       console.log("here", questions[currentStep]);
@@ -82,110 +84,114 @@ const StepForm = props => {
       `;
   };
 
-  //render
-  return questions.length ? (
-    // <div>
-    //   {questions[currentStep].question}
-    //   <input type="text" onChange={(e)=> setAnswer(e.target.value)}/>
-    //   <input type="submit" onClick ={handleSubmit} data-answer={answer}/>
-    // </div>
-    <OnBoardContainer>
-      <StepDots currentDot={StepDotCount} currentStep={currentStep}>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-      </StepDots>
-      <form onSubmit={handleSubmit}>
-        {/* First Question, Concent Button*/}
-        {currentStep < 3 && (
-          <Question>{questions[currentStep].question}</Question>
-        )}
-        {/* First Question, Concent Button*/}
-        {currentStep >= 3 && (
-          <LongQuestion>{questions[currentStep].question}</LongQuestion>
-        )}
-        {/* Returns First and second Question */}
-        {currentStep === 0 && (
-          <>
-            <OnboardTxt>Dont worry, this stays between us</OnboardTxt>
-            <TopArrow>
-              <svg
-                width="19"
-                height="16"
-                viewBox="0 0 19 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17.463 3.10599e-06L1.5392 2.40994e-06C0.324938 2.35686e-06 -0.412842 1.37207 0.248087 2.42873L8.20996 15.2663C8.34859 15.491 8.54036 15.6762 8.76744 15.8044C8.99452 15.9327 9.24954 16 9.50876 16C9.76799 16 10.023 15.9327 10.2501 15.8044C10.4772 15.6762 10.6689 15.491 10.8076 15.2663L18.7541 2.42873C18.904 2.19085 18.9886 1.91586 18.9989 1.63276C19.0092 1.34967 18.9449 1.06896 18.8127 0.820256C18.6805 0.571552 18.4853 0.364073 18.2477 0.219705C18.0102 0.0753375 17.7391 -0.000565655 17.463 3.10599e-06Z"
-                  fill="#E05CB3"
-                />
-              </svg>
-            </TopArrow>
-            <Weight setAnswer={setAnswer} />
-          </>
-        )}
-        {currentStep === 1 && (
-          <>
-            <OnboardTxt>Dont worry, this stays between us</OnboardTxt>
-            <TopArrow>
-              <svg
-                width="19"
-                height="16"
-                viewBox="0 0 19 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17.463 3.10599e-06L1.5392 2.40994e-06C0.324938 2.35686e-06 -0.412842 1.37207 0.248087 2.42873L8.20996 15.2663C8.34859 15.491 8.54036 15.6762 8.76744 15.8044C8.99452 15.9327 9.24954 16 9.50876 16C9.76799 16 10.023 15.9327 10.2501 15.8044C10.4772 15.6762 10.6689 15.491 10.8076 15.2663L18.7541 2.42873C18.904 2.19085 18.9886 1.91586 18.9989 1.63276C19.0092 1.34967 18.9449 1.06896 18.8127 0.820256C18.6805 0.571552 18.4853 0.364073 18.2477 0.219705C18.0102 0.0753375 17.7391 -0.000565655 17.463 3.10599e-06Z"
-                  fill="#E05CB3"
-                />
-              </svg>
-            </TopArrow>
-            <Height setAnswer={setAnswer} />
-          </>
-        )}
-        {/* WILL GIVE OVERWHELMED OPTIONS, Question 3*/}
-        {currentStep === 2 && (
-          <FlexHolder>
-            <Option onClick={() => setAnswer("Never")}>Never</Option>
-            <Option onClick={() => setAnswer("Sometimes")}>Sometimes</Option>
-            <Option onClick={() => setAnswer("Always")}>Always</Option>
-          </FlexHolder>
-        )}
-        {/* Slider Group , Questions 4 - 10*/}
-        {currentStep >= 3 && (
-          <StyledSlider
-            defaultValue={3}
-            max={7}
-            renderTrack={Track}
-            renderThumb={Thumb}
-            onChange={handleChanges}
-          />
-        )}
-        <Button onClick={handleSubmit} data-answer={answer}>
-          Continue
-        </Button>
-        {/* Bypass weight and height answering, Questions 1 and 2 */}
-        {currentStep <= 1 && (
-          <ButtonNoColor onClick={handleSubmit} data-answer={answer}>
-            I don't feel comfortable answering
-          </ButtonNoColor>
-        )}
-      </form>
-    </OnBoardContainer>
-  ) : (
-    <OnBoardContainer>
-      <LoadingSpinner />
-    </OnBoardContainer>
-  );
+  if (done) {
+    return <Redirect to="/dashboard" />;
+  } else {
+    //render
+    return questions.length ? (
+      // <div>
+      //   {questions[currentStep].question}
+      //   <input type="text" onChange={(e)=> setAnswer(e.target.value)}/>
+      //   <input type="submit" onClick ={handleSubmit} data-answer={answer}/>
+      // </div>
+      <OnBoardContainer>
+        <StepDots currentDot={StepDotCount} currentStep={currentStep}>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </StepDots>
+        <form onSubmit={handleSubmit}>
+          {/* First Question, Concent Button*/}
+          {currentStep < 3 && (
+            <Question>{questions[currentStep].question}</Question>
+          )}
+          {/* First Question, Concent Button*/}
+          {currentStep >= 3 && (
+            <LongQuestion>{questions[currentStep].question}</LongQuestion>
+          )}
+          {/* Returns First and second Question */}
+          {currentStep === 0 && (
+            <>
+              <OnboardTxt>Dont worry, this stays between us</OnboardTxt>
+              <TopArrow>
+                <svg
+                  width="19"
+                  height="16"
+                  viewBox="0 0 19 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.463 3.10599e-06L1.5392 2.40994e-06C0.324938 2.35686e-06 -0.412842 1.37207 0.248087 2.42873L8.20996 15.2663C8.34859 15.491 8.54036 15.6762 8.76744 15.8044C8.99452 15.9327 9.24954 16 9.50876 16C9.76799 16 10.023 15.9327 10.2501 15.8044C10.4772 15.6762 10.6689 15.491 10.8076 15.2663L18.7541 2.42873C18.904 2.19085 18.9886 1.91586 18.9989 1.63276C19.0092 1.34967 18.9449 1.06896 18.8127 0.820256C18.6805 0.571552 18.4853 0.364073 18.2477 0.219705C18.0102 0.0753375 17.7391 -0.000565655 17.463 3.10599e-06Z"
+                    fill="#E05CB3"
+                  />
+                </svg>
+              </TopArrow>
+              <Weight setAnswer={setAnswer} />
+            </>
+          )}
+          {currentStep === 1 && (
+            <>
+              <OnboardTxt>Dont worry, this stays between us</OnboardTxt>
+              <TopArrow>
+                <svg
+                  width="19"
+                  height="16"
+                  viewBox="0 0 19 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.463 3.10599e-06L1.5392 2.40994e-06C0.324938 2.35686e-06 -0.412842 1.37207 0.248087 2.42873L8.20996 15.2663C8.34859 15.491 8.54036 15.6762 8.76744 15.8044C8.99452 15.9327 9.24954 16 9.50876 16C9.76799 16 10.023 15.9327 10.2501 15.8044C10.4772 15.6762 10.6689 15.491 10.8076 15.2663L18.7541 2.42873C18.904 2.19085 18.9886 1.91586 18.9989 1.63276C19.0092 1.34967 18.9449 1.06896 18.8127 0.820256C18.6805 0.571552 18.4853 0.364073 18.2477 0.219705C18.0102 0.0753375 17.7391 -0.000565655 17.463 3.10599e-06Z"
+                    fill="#E05CB3"
+                  />
+                </svg>
+              </TopArrow>
+              <Height setAnswer={setAnswer} />
+            </>
+          )}
+          {/* WILL GIVE OVERWHELMED OPTIONS, Question 3*/}
+          {currentStep === 2 && (
+            <FlexHolder>
+              <Option onClick={() => setAnswer("Never")}>Never</Option>
+              <Option onClick={() => setAnswer("Sometimes")}>Sometimes</Option>
+              <Option onClick={() => setAnswer("Always")}>Always</Option>
+            </FlexHolder>
+          )}
+          {/* Slider Group , Questions 4 - 10*/}
+          {currentStep >= 3 && (
+            <StyledSlider
+              defaultValue={3}
+              max={7}
+              renderTrack={Track}
+              renderThumb={Thumb}
+              onChange={handleChanges}
+            />
+          )}
+          <Button onClick={handleSubmit} data-answer={answer}>
+            Continue
+          </Button>
+          {/* Bypass weight and height answering, Questions 1 and 2 */}
+          {currentStep <= 1 && (
+            <ButtonNoColor onClick={handleSubmit} data-answer={answer}>
+              I don't feel comfortable answering
+            </ButtonNoColor>
+          )}
+        </form>
+      </OnBoardContainer>
+    ) : (
+      <OnBoardContainer>
+        <LoadingSpinner />
+      </OnBoardContainer>
+    );
+  }
 };
 
 // STYLED COMPONENTS
