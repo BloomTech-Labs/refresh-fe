e7a798f9e765a96d85e14d2f1545bee0f9d45ffd
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-12 20:06:56 -0700

    small change to ActivityWidget style

--- src/views/profileViews/ActivityCard.js
+++ src/views/profileViews/ActivityCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import LineChart from './profileViews/LineChart';
+import LineChart from './profileViews/'
 
 
 const ActivityWidget = styled.div`
