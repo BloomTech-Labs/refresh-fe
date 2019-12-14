9894f5fed0fa066618ae3060957fbacbd7142f42
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-14 11:35:46 -0700

    update to weekly survey per figma

--- src/views/profileViews/WeeklySurvey.js
+++ src/views/profileViews/WeeklySurvey.js
@@ -1,49 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
-import Rocket from '../../images/profile/rocket.svg';
 
-const SurveyWidget = styled.div`
-    width: 90%;
-    height: 15rem;
-    margin: 2rem 0;
-    background-color: #6762E3;
-    box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
-    border-radius: 2px;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: center;
-    align-items: center;
-    color: rgba(255, 255, 255, 0.6);
 
-        h3 {
-            font-size: 1.25rem;
-            margin-top: auto;
-        }
+const SurveyContainer = styled.div`
+position: absolute;
+width: 162px;
+height: 149px;
+left: 31px;
+top: 614px;
+/* main / side */
 
-        span {
-            margin-bottom: 2rem;
-        }
+background: #3D3B91;
+/* mobile / dashboard dropshadow */
+
+box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
+border-radius: 2px;
+`
+const SurveyText = styled.text`
+position: absolute;
+left: 0%;
+right: 0.66%;
+top: 73.83%;
+bottom: 12.08%;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: normal;
+font-size: 13px;
+line-height: 21px;
+text-align: center;
+letter-spacing: 0.035em;
+
+/* primary / disabled */
+
+color: #CCC9FF;
 `
 
-const SurveyImg = styled.img`
-    width: 45%;
-    height: auto;
-    margin-top: auto;
+const RocketVector = styled.div`
+position: absolute;
+left: 23.03%;
+right: 22.37%;
+top: 12.75%;
+bottom: 31.54%;
+
+background: #FFFFFF;
 `
 
-
-//This component tracks user weekly self-assessments
-const WeeklySurvey = () => {
-    return (
-        <>
-        <SurveyWidget>
-            <SurveyImg src={Rocket} />
-            <h3>Survey of the Week</h3>
-            <span>0% complete</span>
-        </SurveyWidget>
-        </>
-    );
-    
+const WeeklySurvey = () =>{
+  return (
+    <>
+      <SurveyContainer>
+        <RocketVector/>
+        <SurveyText>surveys of the week</SurveyText>
+      </SurveyContainer>
+    </>
+  )
 };
 
 export default WeeklySurvey;
