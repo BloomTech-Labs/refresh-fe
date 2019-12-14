72201b3d7ba393410ece2e157aa7574eb88b036e
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-14 11:34:21 -0700

    add profile header per figma update

---
+++ src/views/profileViews/ProfileHeader.js
@@ -0,0 +1,195 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProfileContainer = styled.div`
+height: 193px;
+`
+const BellVector = styled.div`
+position: absolute;
+width: 25px;
+height: 25px;
+left: 288px;
+top: 21px;
+background: #CCC9FF;
+`
+const CogVector = styled.div`
+position: absolute;
+left: 88.53%;
+right: 6.4%;
+top: 2.71%;
+bottom: 94.95%;
+
+/* main / disabled */
+
+background: #CCC9FF;
+`
+const MenuPlaceHolder = styled.div`
+position: absolute;
+width: 15px;
+height: 15px;
+left: 32px;
+top: 62px;
+
+/* primary / disabled */
+
+background: #CCC9FF;
+`
+const AvPlaceHolder = styled.div`
+position: absolute;
+width: 64px;
+height: 64px;
+left: 160px;
+top: 51px;
+background:#C4C4C4;
+`
+const NameText = styled.text`
+position: absolute;
+width: 124px;
+height: 9px;
+left: 125px;
+top: 131px;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 44%;
+/* identical to box height, or 9px */
+
+
+/* main / TEXT */
+
+color: #E6E6E6;
+`
+const TeamLeadText = styled.div `
+position: absolute;
+width: 75px;
+height: 11px;
+left: 118px;
+top: 155px;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: bold;
+font-size: 12px;
+line-height: 93.19%;
+/* or 11px */
+
+display: flex;
+align-items: flex-end;
+
+/* button / primary color */
+
+color: #E05CB3;
+`
+const PointsText = styled.div`
+position: absolute;
+width: 54px;
+height: 10px;
+left: 202px;
+top: 155px;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: 500;
+font-size: 11px;
+line-height: 93.19%;
+/* or 10px */
+
+display: flex;
+align-items: flex-end;
+letter-spacing: 0.065em;
+
+/* main / menu text */
+
+color: #B8B7E1;
+`
+
+const LvOneText = styled.text`
+position: absolute;
+width: 35px;
+height: 12px;
+left: 62px;
+top: 179px;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: normal;
+font-size: 13px;
+line-height: 93.19%;
+/* or 12px */
+
+display: flex;
+align-items: flex-end;
+letter-spacing: 0.04em;
+
+/* main / menu text */
+
+color: #B8B7E1;
+`
+const LvTwoText = styled.div`
+position: absolute;
+width: 32px;
+height: 12px;
+left: 287px;
+top: 179px;
+
+font-family: Catamaran;
+font-style: normal;
+font-weight: normal;
+font-size: 13px;
+line-height: 93.19%;
+/* or 12px */
+
+display: flex;
+align-items: flex-end;
+letter-spacing: 0.04em;
+
+/* main / menu text */
+
+color: #B8B7E1;
+`
+const MockSlideOne = styled.div`
+position: absolute;
+width: 160px;
+height: 7px;
+left: 107px;
+top: 183px;
+
+background: #E0B8D3;
+opacity: 0.6;
+border-radius: 3px;
+`
+const MockSlideTwo = styled.div`
+position: absolute;
+width: 52px;
+height: 7px;
+left: 107px;
+top: 183px;
+
+/* button / primary color */
+
+background: #E05CB3;
+border-radius: 3px;
+`
+const ProfileHeader = () => {
+  return (
+    <>
+      <ProfileContainer>
+        <BellVector/>
+        <CogVector/>
+        <MenuPlaceHolder/>
+        <AvPlaceHolder/>
+        <NameText>Maxine Woods</NameText>
+        <TeamLeadText>TEAM LEAD</TeamLeadText>
+        <PointsText>150 points</PointsText>
+        <LvOneText>Lvl 1</LvOneText>
+        <LvTwoText>Lvl 2</LvTwoText>
+        <MockSlideOne/>
+        <MockSlideTwo/>
+      </ProfileContainer>
+    </>
+  )
+};
+
+export default ProfileHeader;
