344947fb6e1d6b6f597fb4049db5419aee04e131
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-14 11:36:21 -0700

    update to reminders per figma

--- src/views/profileViews/YourReminders.js
+++ src/views/profileViews/YourReminders.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ReminderText = styled.text`
+position: absolute;
+left: 8.43%;
+right: 33.15%;
+top: 74.11%;
+bottom: 13.39%;
 
-const ContactWidget = styled.div`
-    width: 100%;
-    height: 13rem;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: center;
-    align-items: center;
-    background-color: #CCC9FF;
-    box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
-    border-radius: 2px;
+font-family: Catamaran;
+font-style: normal;
+font-weight: normal;
+font-size: 15px;
+line-height: 97.5%;
+/* or 15px */
 
-        span:first-child {
-            align-self: flex-end;
-            background: none;
-            border: none;
-            font-size: 3rem;
-            color: #6762E3;
-            margin-bottom: auto;
-            margin: 1rem 1rem auto 0;
-            line-height: 0;
-        }
 
-        span:last-child {
-            align-self: flex-start;
-            color: #6762E3;
-            font-size: 1.5rem;
-            margin: 0 0 1rem 1rem;
-        }
+/* main / side */
+
+color: #3D3B91;
 `
-//This component issues missions reminders to user
-const YourReminders = () => {
-    return (
-        <>
-        <ContactWidget>
-            <span>...</span>
-            <span>your reminders</span>
-        </ContactWidget>
-        </>
-    );
-};
 
+const ReminderVector = styled.div`
+position: absolute;
+left: 83.97%;
+right: 5.34%;
+top: 9.15%;
+bottom: 88.73%;
+
+/* main / side */
+
+background: #3D3B91;
+`
+
+const ReminderContainer = styled.div`
+position: absolute;
+width: 131px;
+height: 142px;
+left: 212px;
+top: 621px;
+background: #CCC9FF;
+border-radius: 2px;
+
+`
+
+const YourReminders = () =>{
+  return (
+    <>
+    <ReminderContainer>
+       <ReminderVector/>
+       <ReminderText>your<br/>reminders</ReminderText>
+    </ReminderContainer>
+    </>
+  )
+};
 
 export default YourReminders;
