7db0cf4617630851d3c6371e09bdde6f3b521133
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-14 10:09:31 -0700

    minor change to activity card

--- src/views/profileViews/ActivityCard.js
+++ src/views/profileViews/ActivityCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import LineChart from './profileViews/'
+import LineChart from '../profileViews/LineChart';
 
 
 const ActivityWidget = styled.div`
