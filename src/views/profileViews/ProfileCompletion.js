99634d9ba88e41796b48ce89adab6a0a8d80f823
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2019-12-15 09:23:01 -0700

    remove av for profile completion to fix sizing

--- src/views/profileViews/ProfileCompletion.js
+++ src/views/profileViews/ProfileCompletion.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import User from '../../images/profile/user.svg';
+//import User from '../../images/profile/user.svg';
 
 const ProfileContainer = styled.div`
   position: absolute;
@@ -142,7 +142,7 @@ const ProfileCompletion = () => {
     <>
       <ProfileContainer>
         <ProfileText>profile</ProfileText>
-        <Vector src={User}/>
+        <Vector />
         <Percentage>75%</Percentage>
         <CompletionText>completion</CompletionText>
         <FinishNowContainer>
