c985801935667291c3f8622e759fc604665c7d5b
Josh Cooter <thejoshcooter@gmail.com>
2019-12-16 12:06:03 -0600

    add team view files and stub out component

---
+++ src/views/team-view/TeamView.js
@@ -0,0 +1,23 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const TeamView = () => {
+    return (
+        <>
+            hello from team view
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// TODO
+
+
+// EXPORT
+export default TeamView;
