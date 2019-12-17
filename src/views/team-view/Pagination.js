f0d50e68116143e0e237b2ef4cb5ad3545706608
Josh Cooter <thejoshcooter@gmail.com>
2019-12-17 13:16:25 -0600

    stub out pagination.js

---
+++ src/views/team-view/Pagination.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const Pagination = () => {
+    return (
+        <>
+        hi from pagination
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// TODO
+
+// EXPORTS
+export default Pagination;
