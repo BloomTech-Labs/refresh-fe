7b0997241edf6bdc1e2afb080ef4173de71c07d7
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-12-18 16:51:29 -0800

    survey form submits to be and receives 201, still some features to iron out

--- src/views/survey/CreateSurvey.js
+++ src/views/survey/CreateSurvey.js
@@ -4,68 +4,84 @@ import styled from "styled-components";
 import { axiosWithAuth } from "../../helpers/axiosWithAuth";
 import axios from "axios";
 
+
 const SurveyForm = () => {
   const [form, setForm] = useState({});
-
-  const postData = axios.post("https://apidevnow.com/questiongroups", form);
+  console.log(form)
+  //   const [questions, setQuestions] = useState([]);
 
   const handleChange = e => {
-    setForm({
+    setForm({ ...form,
       [e.target.name]: e.target.value
     });
   };
 
+  //   const handleQuestions = () => {
+  //       setQuestions([...questions, e.])
+  //   }
+
   const handleSubmit = e => {
+      e.preventDefault();
+      console.log("form before object", form)
+    
+    Object.keys(form).forEach(key => {
+      if (key.includes("question")) {
+         
+        form.question_ids=[...(form[key])];
+      }
+    });
+     console.log("form", form);
     e.preventDefault();
-    postData();
+    axiosWithAuth()
+      .post("https://apidevnow.com/questiongroups", form)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   return (
-    <>
-      <StyledWrapper>
-        <StyledForm onSubmit={handleSubmit}>
-          <label>SURVEY NAME</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Create a name for this mission"
-            onChange={handleChange}
-          />
-          <label>SURVEY DESCRIPTION</label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Add a description for your mission..."
-            onChange={handleChange}
-          />
-          <label>QUESTION 1</label>
-          <input
-            type="text"
-            name="question"
-            placeholder="What question would you like to ask?"
-            onChange={handleChange}
-          />
-          <label>QUESTION TYPE</label>
-          <select id="question_type">
-            <option value="null">Please Select One</option>
-            <option value="multiple choice">Multiple Choice</option>
-            <option value="slider">Slider</option>
-            <option value="text reply">Text Reply</option>
-          </select>
-          <button type="submit">Submit</button>
-        </StyledForm>
-      </StyledWrapper>
-    </>
+    <StyledWrapper>
+      <StyledForm onSubmit={handleSubmit}>
+        <label>SURVEY NAME</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Create a name for this mission"
+          onChange={handleChange}
+        />
+        <label>SURVEY DESCRIPTION</label>
+        <input
+          type="text"
+          name="description"
+          placeholder="Add a description for your mission..."
+          onChange={handleChange}
+        />
+        <label>QUESTION 1</label>
+        <input
+          type="text"
+          name="question_1"
+          placeholder="What question would you like to ask?"
+          onChange={handleChange}
+        />
+        <label>QUESTION TYPE</label>
+        <select id="question_type">
+          <option value="">Please Select One</option>
+          <option value="multiple choice">Multiple Choice</option>
+          <option value="slider">Slider</option>
+          <option value="text reply">Text Reply</option>
+        </select>
+        <button type="submit">Submit</button>
+      </StyledForm>
+    </StyledWrapper>
     //styled comp
   );
 };
 
 const StyledForm = styled.form`
-  display: flex;
-  margin: 0 auto;
   flex-direction: column;
+  margin-top: 25%;
 `;
 const StyledWrapper = styled.div`
+  display: flex;
   margin: 0 auto;
   max-width: 500px;
 `;
