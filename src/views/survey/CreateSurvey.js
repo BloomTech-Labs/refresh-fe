1b0441cac82dc2649444fd86e2e7e02841d4f39f
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-12-18 17:37:48 -0800

    dynamically rendering question numbers and id's

--- src/views/survey/CreateSurvey.js
+++ src/views/survey/CreateSurvey.js
@@ -4,16 +4,13 @@ import styled from "styled-components";
 import { axiosWithAuth } from "../../helpers/axiosWithAuth";
 import axios from "axios";
 
-
 const SurveyForm = () => {
   const [form, setForm] = useState({});
-  console.log(form)
+  console.log(form);
   //   const [questions, setQuestions] = useState([]);
 
   const handleChange = e => {
-    setForm({ ...form,
-      [e.target.name]: e.target.value
-    });
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   //   const handleQuestions = () => {
@@ -21,54 +18,58 @@ const SurveyForm = () => {
   //   }
 
   const handleSubmit = e => {
-      e.preventDefault();
-      console.log("form before object", form)
-    
+    e.preventDefault();
+    console.log("form before object", form);
+
     Object.keys(form).forEach(key => {
       if (key.includes("question")) {
-         
-        form.question_ids=[...(form[key])];
+        form.question_ids = [...form[key]];
       }
     });
-     console.log("form", form);
+    console.log("form", form);
     e.preventDefault();
     axiosWithAuth()
       .post("https://apidevnow.com/questiongroups", form)
       .then(res => console.log(res))
       .catch(err => console.log(err));
+    count = 0;
   };
+  let count = 1;
+  const questionCount = count++;
 
   return (
     <StyledWrapper>
       <StyledForm onSubmit={handleSubmit}>
-        <label>SURVEY NAME</label>
+        <h3>SURVEY NAME</h3>
         <input
           type="text"
           name="name"
           placeholder="Create a name for this mission"
           onChange={handleChange}
         />
-        <label>SURVEY DESCRIPTION</label>
+        <h3>SURVEY DESCRIPTION</h3>
         <input
           type="text"
           name="description"
           placeholder="Add a description for your mission..."
           onChange={handleChange}
         />
-        <label>QUESTION 1</label>
+        <h3>QUESTION {questionCount}</h3>{" "}
         <input
           type="text"
-          name="question_1"
+          name={`question_` + questionCount}
           placeholder="What question would you like to ask?"
           onChange={handleChange}
         />
-        <label>QUESTION TYPE</label>
-        <select id="question_type">
-          <option value="">Please Select One</option>
-          <option value="multiple choice">Multiple Choice</option>
-          <option value="slider">Slider</option>
-          <option value="text reply">Text Reply</option>
-        </select>
+        <h3>
+          QUESTION TYPE
+          <select id="question_type">
+            <option value="">Please Select One</option>
+            <option value="multiple choice">Multiple Choice</option>
+            <option value="slider">Slider</option>
+            <option value="text reply">Text Reply</option>
+          </select>
+        </h3>
         <button type="submit">Submit</button>
       </StyledForm>
     </StyledWrapper>
