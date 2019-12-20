6326ade7c22e2daa57442a58ca3f428b08995add
Josh Cooter <thejoshcooter@gmail.com>
2019-12-20 04:16:07 -0600

    stub out main menu component

---
+++ src/views/mobile-menu/MainMenu.js
@@ -0,0 +1,22 @@
+// IMPORTS
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { test, flex } from '../../styles/global/Mixins';
+
+// COMPONENT
+const MainMenu = () => {
+    return (
+        <>
+        hi from main menu
+        </>
+    );
+};
+
+// STYLED COMPONENTS
+// todo
+
+// EXPORT
+export default MainMenu;
