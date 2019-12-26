058cf5b6066738ab492ee458ffa4c305fce6c3cd
roman-jordan <rjordan1352@gmail.com>
2019-12-26 12:24:57 -0800

    Fixed Context Issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import PrivateRoute from "./helpers/PrivateRoute";
 import PrivateViewCanvas from "./views/private";
 import PublicViewCanvas from "./views/public";
 
 const App = props => {
-  const [userStatus,setUserStatus] = useState(false)
+  const [userStatus, setUserStatus] = useState(false);
   return (
-    <PrivateRoute component={PrivateViewCanvas} public={PublicViewCanvas} userStatus={setUserStatus} />  );
+    <PrivateRoute
+      component={PrivateViewCanvas}
+      public={PublicViewCanvas}
+      userStatus={setUserStatus}
+    />
+  );
 };
 
 export default App;
