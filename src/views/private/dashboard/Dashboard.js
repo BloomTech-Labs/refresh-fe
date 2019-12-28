f276157873f47b5e56230bceff7c022aa117e549
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-12-27 19:37:51 -0500

    Built progress bar component, added to Dasboard.

--- src/views/private/dashboard/Dashboard.js
+++ src/views/private/dashboard/Dashboard.js
@@ -33,8 +33,7 @@ const Dashboard = props => {
         <DashboardContainer>
           <Header>
             <Greeting>
-              <h1>Hello</h1>
-              <h1>{activeUser.fname}</h1>
+              <h1>Hello {activeUser.fname}!</h1>
             </Greeting>
 
             <User>
@@ -56,6 +55,8 @@ const Dashboard = props => {
 
           <h2 className="mission-message">Your missions today</h2>
 
+          <ProgressComponent missions={userMissions} />
+
           <MissionHub>
             {userMissions.map((mission, i) => {
               return (
@@ -74,11 +75,11 @@ const Dashboard = props => {
           </MissionHub>
 
           <AddMission>
-          <StartTimer>
-            <p>
-              Ready to work?<br></br>
-              <Link to="/timer">Start Pomodoro</Link>
-            </p>
+            <StartTimer>
+              <p>
+                Ready to work?<br></br>
+                <Link to="/timer">Start Pomodoro</Link>
+              </p>
             </StartTimer>
             <Link to="/mission-complete">
               <MissionButton>+</MissionButton>
@@ -125,17 +126,22 @@ const Header = styled.div`
 
 const Greeting = styled.div`
   color: #fff;
-
+  text-align: center;
   h1 {
     font-size: 4.5rem;
     letter-spacing: 0.25rem;
     font-weight: bold;
+    width: 90vw;
+    margin: auto;
   }
 `;
 
 const User = styled.div`
   width: 10rem;
   height: 5rem;
+  position: absolute;
+  top: 3rem;
+  right: 3rem;
   ${flex.flexRowNoWrapAround}
 
   i {
@@ -161,6 +167,8 @@ const Avatar = styled.div`
 
 const MissionHub = styled.div`
   width: 100%;
+  max-height: 42vh;
+  overflow-y: scroll;
   ${flex.flexRowWrapAround}
 `;
 
@@ -182,7 +190,7 @@ const MissionButton = styled.button`
   width: 56px;
   height: 56px;
   margin-left: auto;
-  background-color: #6487ff;
+  background-color: #fc54eb;
   border: none;
   border-radius: 3px;
   box-shadow: 0px 4px 10px rgba(21, 15, 172, 0.1);
@@ -195,10 +203,69 @@ const StartTimer = styled.div`
   font-size: 1.6rem;
   line-height: 125.5%;
   a {
-      text-decoration: none;
-      color: #fff;
+    text-decoration: none;
+    color: #fff;
   }
-`
+`;
+
+const ProgressComponent = props => {
+  console.log(props.missions);
+  const { missions } = props;
+  function missionCount() {
+    let currentPoints = 0;
+    let goalCount = 0;
+    missions.forEach((mission, i) => {
+        mission.point_current = 5;
+        goalCount = goalCount + mission.goal;
+        currentPoints = currentPoints + mission.point_current;
+    });
+    console.log('current', currentPoints);
+    console.log('goal', goalCount);
+    let percentComplete = currentPoints / goalCount;
+    return Math.round(percentComplete * 100);
+  }
+  const percentComplete = missionCount();
+  return (
+    <ProgressBar width={percentComplete}>
+      <p>{percentComplete}% Complete</p>
+      <div><div><div></div></div></div>
+    </ProgressBar>
+  );
+};
+
+const ProgressBar = styled.div`
+  height: 10vh;
+  width: 90vw;
+  margin: auto;
+  p {
+    font-family: Catamaran;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 2.5rem;
+    color: #E05CB3;
+  }
+  div {
+      height: 1rem;
+      width: 100%;
+      border-radius: 5rem;
+      background: #5E5CBD;
+      div {
+          width: ${props => props.width}%;
+          height: 1rem;
+          background: #E05CB3;
+          position: relative;
+          div {
+            background: #E77EC3;
+            border-radius: 50%;
+            position: absolute;
+            right: 0;
+            width: 12px;
+            height: 12px;
+          }
+      }
+  }
+`;
 
 // EXPORT
 export default Dashboard;
