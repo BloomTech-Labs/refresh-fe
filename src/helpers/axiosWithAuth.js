86f70a647016ed74f55ae93e34d61992dac053a7
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-12-28 12:26:05 -0500

    Updated baseURL.

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -6,12 +6,10 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: `http://localhost:8080`,
+        baseURL: `https://refresh-yo-beta.herokuapp.com`,
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${token}`
         }
     });
 };
-
-//https://refresh-yo-beta.herokuapp.com
