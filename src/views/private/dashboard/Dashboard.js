e971c8d10bfb8c6e34e05912b8d2d1e9eedcd05f
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-12-27 19:41:50 -0500

    Split into its own component so nobody gets confused.

--- src/views/private/dashboard/Dashboard.js
+++ src/views/private/dashboard/Dashboard.js
@@ -12,6 +12,7 @@ import styled from "styled-components";
 import { test, flex } from "../../../styles/global/Mixins";
 // components
 import MissionCard from "./MissionCard";
+import Progress from "./Progress";
 
 // COMPONENT
 const Dashboard = props => {
@@ -55,7 +56,7 @@ const Dashboard = props => {
 
           <h2 className="mission-message">Your missions today</h2>
 
-          <ProgressComponent missions={userMissions} />
+          <Progress missions={userMissions} />
 
           <MissionHub>
             {userMissions.map((mission, i) => {
@@ -205,65 +206,6 @@ const StartTimer = styled.div`
   a {
     text-decoration: none;
     color: #fff;
-  }
-`;
-
-const ProgressComponent = props => {
-  console.log(props.missions);
-  const { missions } = props;
-  function missionCount() {
-    let currentPoints = 0;
-    let goalCount = 0;
-    missions.forEach((mission, i) => {
-        mission.point_current = 5;
-        goalCount = goalCount + mission.goal;
-        currentPoints = currentPoints + mission.point_current;
-    });
-    console.log('current', currentPoints);
-    console.log('goal', goalCount);
-    let percentComplete = currentPoints / goalCount;
-    return Math.round(percentComplete * 100);
-  }
-  const percentComplete = missionCount();
-  return (
-    <ProgressBar width={percentComplete}>
-      <p>{percentComplete}% Complete</p>
-      <div><div><div></div></div></div>
-    </ProgressBar>
-  );
-};
-
-const ProgressBar = styled.div`
-  height: 10vh;
-  width: 90vw;
-  margin: auto;
-  p {
-    font-family: Catamaran;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-    color: #E05CB3;
-  }
-  div {
-      height: 1rem;
-      width: 100%;
-      border-radius: 5rem;
-      background: #5E5CBD;
-      div {
-          width: ${props => props.width}%;
-          height: 1rem;
-          background: #E05CB3;
-          position: relative;
-          div {
-            background: #E77EC3;
-            border-radius: 50%;
-            position: absolute;
-            right: 0;
-            width: 12px;
-            height: 12px;
-          }
-      }
   }
 `;
 
---
+++ src/views/private/dashboard/Progress.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Progress = props => {
+    console.log(props.missions);
+    const { missions } = props;
+    function missionCount() {
+      let currentPoints = 0;
+      let goalCount = 0;
+      missions.forEach((mission, i) => {
+          mission.point_current = 5;
+          goalCount = goalCount + mission.goal;
+          currentPoints = currentPoints + mission.point_current;
+      });
+      console.log('current', currentPoints);
+      console.log('goal', goalCount);
+      let percentComplete = currentPoints / goalCount;
+      return Math.round(percentComplete * 100);
+    }
+    const percentComplete = missionCount();
+    return (
+      <ProgressBar width={percentComplete}>
+        <p>{percentComplete}% Complete</p>
+        <div><div><div></div></div></div>
+      </ProgressBar>
+    );
+  };
+  
+  const ProgressBar = styled.div`
+    height: 10vh;
+    width: 90vw;
+    margin: auto;
+    p {
+      font-family: Catamaran;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 1.5rem;
+      line-height: 2.5rem;
+      color: #E05CB3;
+    }
+    div {
+        height: 1rem;
+        width: 100%;
+        border-radius: 5rem;
+        background: #5E5CBD;
+        div {
+            width: ${props => props.width}%;
+            height: 1rem;
+            background: #E05CB3;
+            position: relative;
+            div {
+              background: #E77EC3;
+              border-radius: 50%;
+              position: absolute;
+              right: 0;
+              width: 12px;
+              height: 12px;
+            }
+        }
+    }
+  `;
+
+  export default Progress;
