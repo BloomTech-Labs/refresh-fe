746d467afd32072a64267fe99288e72d3b28ee8f
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-12-31 14:50:59 -0800

    broken, but think the idea is there just need to flesh out

--- src/views/survey/CreateSurvey.js
+++ src/views/survey/CreateSurvey.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import { axiosWithAuth } from "../../helpers/axiosWithAuth";
 import axios from "axios";
 
 const SurveyForm = () => {
-  const [form, setForm] = useState({
-  
-  });
+  const [questions, setQuestions] = useState([]);
+  const [formValues, setFormValues] = useState({});
   const [enabledBtn, setEnabledBtn] = useState(false);
-  console.log(form);
-  //   const [questions, setQuestions] = useState([]);
+
+  console.log(formValues);
+
+  const questionHandleChange = (idx, e) => {
+    const values = [...questions];
+    if (e.target.name.includes("question_")) {
+      values[idx].question = e.target.value;
+    } else {
+      values[idx].questionType = e.target.value;
+    }
+    setQuestions(values);
+  };
 
   const handleChange = e => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
   };
 
   //   const handleQuestions = () => {
@@ -21,37 +30,43 @@ const SurveyForm = () => {
   //   }
   let errors = {};
   useEffect(() => {
-    errors.surveyName = !form.name && "Please enter a name for your survey";
+    errors.surveyName =
+      !formValues.name && "Please enter a name for your survey";
     errors.surveyDesc =
-      !form.description && "Please enter a description for your survey";
+      !formValues.description && "Please enter a description for your survey";
     errors.surveyQType =
-      !form.questionType &&
+      !formValues.questionType &&
       "Please select which type of answers this survey is looking for";
     !errors.surveyName &&
       !errors.surveyDesc &&
       !errors.surveyQType &&
       setEnabledBtn(true);
-  }, [form]);
+  }, [formValues]);
 
-  
+  const addQuestion = () => {
+    console.log("added question");
+    const values = [...questions];
+    values.push({ question: "", questionType: "" });
+    setQuestions(values);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
     if (!enabledBtn) {
       alert(errors.surveyName || errors.surveyDesc || errors.surveyQType);
     } else {
-     
-      Object.keys(form).map(key => {
+      Object.keys(formValues).map(key => {
         if (key.includes("question_")) {
-          console.table(form)
+          console.table(formValues);
           // console.log("question value", form.question_.value )
-           console.log("form{key}", form[key]);
-          form.question_ids = [form[key]];
+          console.log("form{key}", formValues[key]);
+          formValues.question_ids = [formValues[key]];
         }
       });
-      console.log("form", form);
+      console.log("form", formValues);
       e.preventDefault();
       axiosWithAuth()
-        .post("https://apidevnow.com/questiongroups", form)
+        .post("https://apidevnow.com/questiongroups", formValues)
         .then(res => console.log(res))
         .catch(err => console.log(err));
       count = 1;
@@ -63,40 +78,52 @@ const SurveyForm = () => {
   return (
     <StyledWrapper>
       <StyledForm onSubmit={handleSubmit}>
-        <h3>SURVEY NAME</h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Create a name for this mission"
-          onChange={handleChange}
-        />
-        <h3>SURVEY DESCRIPTION</h3>
-        <input
-          type="text"
-          name="description"
-          placeholder="Add a description for your mission..."
-          onChange={handleChange}
-        />
-        <h3>QUESTION {questionCount}</h3>{" "}
-        <input
-          type="text"
-          name={`question_` + questionCount}
-          placeholder="What question would you like to ask?"
-          onChange={handleChange}
-        />
-        <h3>QUESTION TYPE</h3>
-        <select name="questionType" onChange={handleChange}>
-          <option value="">Please Select One</option>
-          <option value="multiple choice">Multiple Choice</option>
-          <option value="slider">Slider</option>
-          <option value="text reply">Text Reply</option>
-        </select>
+        {formValues.map((value, index) => (
+          <Fragment key={`${value}~${index}`}>
+            <>
+              <h3>SURVEY NAME</h3>
+              <input
+                type="text"
+                name="name"
+                value={value.name}
+                placeholder="Create a name for this mission"
+                onChange={handleChange}
+              />
+              <h3>SURVEY DESCRIPTION</h3>
+              <input
+                type="text"
+                name="description"
+                value={value.description}
+                placeholder="Add a description for your mission..."
+                onChange={handleChange}
+              />
+              <h3>QUESTION {questionCount}</h3>
+              <input
+                type="text"
+                name={`question_` + questionCount}
+                placeholder="What question would you like to ask?"
+                onChange={questionHandleChange}
+              />
+              <h3>QUESTION TYPE</h3>
+              <select name="questionType" onChange={questionHandleChange}>
+                <option value="">Please Select One</option>
+                <option value="multiple choice">Multiple Choice</option>
+                <option value="slider">Slider</option>
+                <option value="text reply">Text Reply</option>
+              </select>
+              <div onClick={addQuestion}>Add Question</div>
+            </>
+          </Fragment>
+        ))}
+
         <button type="submit">Submit</button>
       </StyledForm>
     </StyledWrapper>
     //styled comp
   );
 };
+
+const StyledButton = styled.button``;
 
 const StyledForm = styled.form`
   display: flex;
