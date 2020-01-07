d3ec25876925f29fbcac429dee02fb5bec926fec
Gina Christians <chrisgins@Lambda-School-Loaner-31s-MacBook-Pro.local>
2020-01-07 11:05:24 -0700

    deleted challengeBox for troubleshooting gc3 only

--- src/views/private/index.js
+++ src/views/private/index.js
@@ -19,7 +19,6 @@ import Calendar from "./team-view/Calendar";
 import TeamList from "./team-view/TeamList";
 import StepStart from "./firstLogin/StepStart";
 import SurveyCanvas from "./survey";
-import WeeklyChallenge from "./weeklyChallenge";
 import ProfileEdit from "./profileViews/ProfileEdit";
 
 const PrivateViewCanvas = props => {
@@ -47,7 +46,6 @@ const PrivateViewCanvas = props => {
         <Route path="/coming-soon" component={ComingSoon} />
         <Route path="/timer" component={TimerCanvas} />
         <Route path="/survey" component={SurveyCanvas} />
-        <Route path="/weekly-challenges" component={WeeklyChallenge} />
         <Route path="/profile-edit" component={ProfileEdit} />
         <Redirect to="/dashboard" />
       </Switch>
