2985dc7d1d39d47d70dff462913e1505c675e307
roman-jordan <rjordan1352@gmail.com>
2020-01-09 10:40:16 -0800

    Reverted Context

--- src/contexts/ContextRouter.js
+++ src/contexts/ContextRouter.js
@@ -12,9 +12,9 @@ const ContextRouter = ({
   debug,
   ...rest
 }) => {
-  let [userMissions, setUserMissions] = useState([]);
+  const [userMissions, setUserMissions] = useState([]);
   const [user, setUser] = useState();
-  let [team, setTeam] = useState();
+  const [team, setTeam] = useState();
 
   useEffect(() => {
     !user &&
@@ -27,19 +27,17 @@ const ContextRouter = ({
             mission_subscriptions,
             missions_in_progress
           } = res.data.user_missions;
-
           const userRole = res.data.user_profile.user_roles;
           const roleTitle = userRole ? userRole[userRole.length - 1].role : "";
 
           mission_subscriptions = Array.isArray(missions_in_progress)
             ? ctx.missionMasher(mission_subscriptions, missions_in_progress)
             : mission_subscriptions;
-          
-          //Sets Default Context
-          userMissions = mission_subscriptions;
-          team = res.data.my_teams[0]
-          //set last
-          setUser({...res.data.user_profile,roleTitle}) 
+       
+          setUser({...res.data.user_profile,roleTitle});
+          setUserMissions(mission_subscriptions);
+          setTeam(res.data.my_teams[0])
+   
         })
         .catch(err => {
           console.log(err);
