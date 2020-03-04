bfad63aa24f6f296d976b4cebbb87ae754b174fc
coltynw <coltynw@users.github.com>
2020-03-04 13:12:46 -0500

    update

--- src/views/public/AdminNav.js
+++ src/views/public/AdminNav.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//images
+import logo from './AdminNavImages/logo.png';
+import leaderboard from './AdminNavImages/leaderboard.png';
+import user from './AdminNavImages/user.png';
+import settings from './AdminNavImages/settings.png';
+import logout from './AdminNavImages/signout 1.png';
 
 export const NavBar = styled.div`      
-    background-color: blue;
+    background-color: rgb(61,162,237);
     color: white;
     text-align: center;
     display: flex;
     flex-direction: column;
-    width: 20%;
+    width: 10vh;
     height: 100vh;
     justify-content: space-between;
+    .img {
+        display: flex;
+        justify-content: space-between;
+        opacity: 70%; 
+        width:  5vh;
+        height: 5vh;
+        margin-left: 2.5vh;
+
+        &:hover {
+        opacity: 100%; 
+      }
+    }
     
 `;
 
@@ -19,51 +37,41 @@ export const Content = styled.div`
     background-color: white;
 `;
 
-export const Stars = styled.image`
-    // oijofjiwifj
+ export const Stars = styled.img`
+    margin-top: 2vh;
+    margin-bottom: 15vh;
+ `;
+
+
+export const Leaderboard = styled.img`
 
 `;
 
-export const Leaderboard = styled.image`
-    // oijofjiwifj
-
-`;
-
-export const Users = styled.image`
-    // oijofjiwifj
-
-`;
-
-
-export const Settings = styled.image`
-    // oijofjiwifj
+export const Users = styled.img`
 
 `;
 
 
-export const Logout = styled.image`
-    // oijofjiwifj
+export const Settings = styled.img`
 
+`;
+
+
+export const Logout = styled.img`
+    margin-top: 35vh;
+    margin-bottom: 2vh;
 `;
 
 function WithNavigation (props) {
     return <div>
         <NavBar>
-            <Stars>
-                STAR IMAGE HERE
-            </Stars>
-            <Leaderboard>
-                LEADERBOARD
-            </Leaderboard>
-            <Users>
-                USERS
-            </Users>
-            <Settings>
-                Settings
-            </Settings>
-            <Logout>
-                logout
-            </Logout>
+            <Stars className='img' src={logo} alt='stars'/>
+            <div></div>
+            <Leaderboard className='img'src={leaderboard} alt='leaderboard'/>
+            <Users className='img' src={user} alt='user'/>
+            <Settings className='img' src={settings} alt='settings'/>
+            <div></div>
+            <Logout className='img' src={logout} alt='logout'/>
         </NavBar>
         <Content>
             {
