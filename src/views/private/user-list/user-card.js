791232eddd5d3777f664b7320096c8b09ddd24bd
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-09 09:46:30 -0500

    rendering all user cards

---
+++ src/views/private/user-list/user-card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProfileCard = styled.div`
+background: white;
+width: 250px;
+padding: 30px;
+border-radius: 3px;
+margin: 30px;
+`
+
+
+const UserCard = props => {
+    console.log('UserCard', props.info.first_name)
+
+
+    return (
+        <div>
+            <ProfileCard>
+                <h2>{`${props.info.first_name} ${props.info.last_name}`}</h2>
+                <h2>Team: {props.info.team_id}</h2>
+                <h2>
+                    {`${props.info.points === null ? props.info.points = 0 : props.info.points} POINTS`}
+                </h2>
+                <h2>{props.info.email}</h2>
+            </ProfileCard>
+        </div>
+    )
+}
+
+export default UserCard;
--- src/views/private/user-list/user-list.js
+++ src/views/private/user-list/user-list.js
@@ -2,14 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+import UserCard from './user-card';
+
+const List = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+`
+
 const UserList = props => {
     const [users, setUsers] = useState();
 
     useEffect(() => {
         axios.get('https://labs-refresh.herokuapp.com/users/')
             .then((response) => {
-                const users = response.data;
-                setUsers(users);
+                const usersInfo = response.data;
+                setUsers(usersInfo);
             })
             .catch((error) => {
                 console.log(error)
@@ -22,8 +30,16 @@ const UserList = props => {
     } else {
         return (
             <div>
-                <h1>User Profiles</h1>
-                
+                <h1>All Users</h1>
+                <List>
+                    {users.map((user, key) => {
+                        return (
+                            <div key={user.id}>
+                                <UserCard info={user} />
+                            </div>
+                        )
+                    })}
+                </List>
             </div>
         )
     }
