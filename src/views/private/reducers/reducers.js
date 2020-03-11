d5415488cfa0fb317414d194c4b456bfb7ccb499
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-11 12:37:17 -0500

    added cases for FETCH_ALL_USERS to reducer function

--- src/views/private/reducers/reducers.js
+++ src/views/private/reducers/reducers.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_ALL_USERS_LOADING,
+    FETCH_ALL_USERS_SUCCESS,
+    FETCH_ALL_USERS_FAILURE
+} from '../actions/actions';
+
 const initialState = {
     allUsers: [],
     singleUser: {},
@@ -7,6 +13,21 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch(action.type) {
+        case FETCH_ALL_USERS_LOADING:
+            return {
+                ...state,
+                isFetching: true,
+            }
+        case FETCH_ALL_USERS_SUCCESS:
+            return {
+                ...state,
+                allUsers: action.payload
+            }
+        case FETCH_ALL_USERS_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state;
     }
