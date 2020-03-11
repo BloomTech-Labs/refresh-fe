91978b9e9318534e10b131c68d605300060bc554
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-11 12:04:37 -0500

    added actions/reducers folders/files; set up initialState + reducer; created store

---
+++ src/views/private/reducers/reducers.js
@@ -0,0 +1,15 @@
+const initialState = {
+    allUsers: [],
+    singleUser: {},
+    isFetching: false,
+    error: null
+}
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
