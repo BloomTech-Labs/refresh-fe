248d9372eb1f195cb5f3c93569b7832bc8f14b24
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-11 12:29:23 -0500

    added action creator for fetching all users

---
+++ src/views/private/actions/actions.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+// Action Types
+export const FETCH_ALL_USERS_LOADING = 'FETCH_ALL_USERS_LOADING';
+export const FETCH_ALL_USERS_SUCCESS = 'FETCH_ALL_USERS_SUCCESS';
+export const FETCH_ALL_USERS_FAILURE = 'FETCH_ALL_USERS_FAILURE';
+
+
+// Async Action Creators
+export const fetchAllUsers = () => dispatch => {
+    dispatch({ FETCH_ALL_USERS_LOADING })
+    axios
+        .get(`https://labs-refresh.herokuapp.com/users/`)
+        .then(response => dispatch({ type: FETCH_ALL_USERS_SUCCESS, payload: response.data }))
+        .catch(error => dispatch({ type: FETCH_ALL_USERS_FAILURE, payload: error }))
+}
