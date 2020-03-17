7e5584c1e3716fc72ef6df3c9612abba1d9066b6
coltynw <coltynw@users.github.com>
2020-03-17 12:56:20 -0400

    add eam modal styled

--- src/views/private/admin-leaderboard/addTeam.js
+++ src/views/private/admin-leaderboard/addTeam.js
@@ -5,6 +5,95 @@ import TeamCard from './teamCard';
 import { connect } from 'react-redux';
 import { createNewTeam } from '../actions/actions';
 
+import styled from 'styled-components';
+
+
+const Words = styled.div `
+display: flex;
+justify-conent: center;
+float: center;
+
+position: absolute;
+margin-right: 203px;
+margin-left: 303px;
+margin-top: 84px;
+margin-bottom: 195px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: normal;
+font-size: 40px;
+line-height: 47px;
+color: #3B444B;
+`
+const ButtonStyle1 = styled.div `
+width: 113px;
+height: 64px;
+
+position: absolute;
+margin-right: 500px;
+margin-left: 336px;
+margin-top: 242px;
+margin-bottom: 83px;
+text-align: center;
+color: white;
+background: #F0372B;
+border-radius: 4px;
+font-size: 28px;
+font-margin-top: 16px;
+:hover {
+    cursor: pointer;
+    opacity: 50%;
+}
+p {
+    margin-top: 16px;
+}
+
+`
+const ButtonStyle2 = styled.div `
+width: 113px;
+height: 64px;
+
+position: absolute;
+margin-right: 302px;
+margin-left: 500px;
+margin-top: 242px;
+margin-bottom: 83px;
+text-align: center;
+color: white;
+background: #515257;
+border-radius: 4px;
+font-size: 28px;
+font-margin-top: 16px;
+:hover {
+    cursor: pointer;
+    opacity: 50%;
+}
+p {
+    margin-top: 16px;
+}
+`
+const SearchBox = styled.div `
+display: flex;
+justify-conent: center;
+float: center;
+
+position: absolute;
+margin-right: 203px;
+margin-left: 380px;
+margin-top: 150px;
+margin-bottom: 195px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: normal;
+font-size: 40px;
+line-height: 47px;
+color: #3B444B;
+`
+
 Modal.setAppElement('#root')
 
 function AddTeam (props) {
@@ -27,16 +116,16 @@ function AddTeam (props) {
         },
         content: {
             position: 'absolute',
-            top: '150px',
-            left: '250px',
-            right: '250px',
-            bottom: '150px',
+            top: '25vh',
+            left: '20vw',
+            right: '20vw',
+            bottom: '25vh',
             background: '#fff',
             overflow: 'auto',
             WebkitOverflowScrolling: 'touch',
             borderRadius: '10px',
             outline: 'none',
-            padding: '20px',
+            padding: '20px'
             
         },
         body: {
@@ -68,8 +157,8 @@ function AddTeam (props) {
             <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} 
                 style={modalStyle}
                 >
-                <p>Enter a team name</p>
-                
+                <Words>Enter a team name</Words>
+                <SearchBox>
                 <form onSubmit={handleSubmit}>
                     <input
                     id="team"
@@ -79,10 +168,12 @@ function AddTeam (props) {
                     value={teamName.name}
                     onChange={handleChange}
                     />
-                <button >Confirm</button>
+                    </form>
+                    </SearchBox>
+                <ButtonStyle1 ><p>Confirm</p></ButtonStyle1>
                 {/* <button onSubmit={handleSubmit}>Confirm</button> */}
-                </form>
-                <button onClick={() => setModalIsOpen(false)}>Cancel</button>
+                
+                <ButtonStyle2 onClick={() => setModalIsOpen(false)}><p>Cancel</p></ButtonStyle2>
             </Modal>
         </div>
     );
