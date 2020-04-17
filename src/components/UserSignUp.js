e5031e2c5bc263a6ed26d2fdfd196df7132d913d
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-04-17 18:09:52 -0400

    added react-hook-form for form validation on user sign up
    set up react-hook-form and associated functions to validate inputs for sign up. still need to update validate passwords this way
    
    Co-authored-by: Suffyan Tariq <suffyan.tariq@gmail.com

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,86 +1,86 @@
 import React, {useState} from "react";
 import {Link} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
 
 function UserSignUp() {
 
+    const {register, handleSubmit, errors} = useForm();
 
-    const [user, setUser] = useState({
-        fullName: '',
-        email: '',
-        password: '',
-        verifyPassword: ''
-    });
-
-
-    //handle any changes made to input fields email and password
-    const handleChanges = event => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.value
-        })
-    }
-
+    
 
     //handle form submit
-    const submitForm = event => {
+    const submitForm = (data, event) => {
         event.preventDefault();
 
-        if (verifyPass()) {
+        console.log("data from submitForm", data)
+
+
+
+
+        //handle verifying if passwords match
+        if (verifyPass(data)) {
              //add login action for redux 
              console.log("Passwords match!")
         }
         else {
             console.log("Passwords do not match")
-            //add validation response
+
+            //add validation response (whatever we want that to be, pop-up with message, etc)
+
+            //event.target.reset();
         }
 
     }
 
     //handle verifying password
-    const verifyPass = () => {
+    const verifyPass = (user) => {
         return (user.password === user.verifyPassword ? true : false);
     }
 
     return (
         <div>
-            <form onSubmit={submitForm}>
+            <form onSubmit={handleSubmit(submitForm)}>
 
                <label for="fullName">Full Name: </label>
                <input
                     id="fullName"
                     type="text"
                     name="fullName"
-                    onChange={handleChanges}
-                    value={user.fullName}
+                    ref={register({required: "Full name is required"})}
                 />
+
+                {errors.fullName && <p>{errors.fullName.message}</p>}
 
                 <label for="email">Email: </label>
                <input
                     id="email"
                     type="email"
                     name="email"
-                    onChange={handleChanges}
-                    value={user.email}
+                    ref={register({required: "Email address is required", pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "invalid email address"}})}
                 />
+
+                {errors.email && <p>{errors.email.message}</p>}
 
                 <label for="password">Password: </label>
                 <input
                     id="password"
                     type="password"
                     name="password"
-                    onChange={handleChanges}
-                    value={user.password}
+                    ref={register({ required: "Password is required", minLength: {value: 5, message: "Password must be at least 5 letters"} })}
                 />
 
-                <label for="verifyPassword">Password: </label>
+                {errors.password && <p>{errors.password.message}</p>}
+
+                <label for="verifyPassword">Verify password: </label>
                 <input
                     id="verifyPassword"
                     type="password"
                     name="verifyPassword"
-                    onChange={handleChanges}
-                    value={user.verifyPassword}
+                    ref={register({ required: "Please verify your password"})}
                 />
 
+                {errors.verifyPassword && <p>{errors.verifyPassword.message}</p>}
 
                 <button type="submit">Log in</button>
                 
