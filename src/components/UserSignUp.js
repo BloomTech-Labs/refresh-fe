b4c797ece6f11ee76dd40a8278f586884c6bcffe
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-04-17 19:52:02 -0400

    Added functionality for validating password matching through react-hook-form
    Removed the set up that was previously in place that manually checked that passwords matched, and set up the same functionality and more through react-hook-form. Set up state to track the password entered fiword entered first, then validated that the passwords must match on the verifyPassword input. Using validation for this through react-hook-form keeps validation consistent throughout this form since the rest of the fe rest of the form was using it as well, and allows users to see live error message updates for password matching

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -4,37 +4,24 @@ import {useForm} from 'react-hook-form';
 
 function UserSignUp() {
 
-    const {register, handleSubmit, errors} = useForm();
+     //adjusted the 'mode' argument from the default value of 'onSubmit' for the useForm hook, in order to allow live error changes as user types
+    const {register, handleSubmit, errors} = useForm({mode: 'onChange'});
 
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    //handle changes for password field in order to validate password matching
+    const handleChanges = event => {
+        setEnteredPassword(event.target.value);
+    }
     
-
     //handle form submit
     const submitForm = (data, event) => {
         event.preventDefault();
 
         console.log("data from submitForm", data)
 
+        //add login action here for redux, can create new object from 'data' only including email/pass to send to the db
 
-
-
-        //handle verifying if passwords match
-        if (verifyPass(data)) {
-             //add login action for redux 
-             console.log("Passwords match!")
-        }
-        else {
-            console.log("Passwords do not match")
-
-            //add validation response (whatever we want that to be, pop-up with message, etc)
-
-            //event.target.reset();
-        }
-
-    }
-
-    //handle verifying password
-    const verifyPass = (user) => {
-        return (user.password === user.verifyPassword ? true : false);
     }
 
     return (
@@ -51,7 +38,7 @@ function UserSignUp() {
 
                 {errors.fullName && <p>{errors.fullName.message}</p>}
 
-                <label for="email">Email: </label>
+               <label for="email">Email: </label>
                <input
                     id="email"
                     type="email"
@@ -65,8 +52,10 @@ function UserSignUp() {
                 <label for="password">Password: </label>
                 <input
                     id="password"
-                    type="password"
+                    type="text"
                     name="password"
+                    onChange={handleChanges}
+                    defaultValue={enteredPassword}
                     ref={register({ required: "Password is required", minLength: {value: 5, message: "Password must be at least 5 letters"} })}
                 />
 
@@ -75,9 +64,9 @@ function UserSignUp() {
                 <label for="verifyPassword">Verify password: </label>
                 <input
                     id="verifyPassword"
-                    type="password"
+                    type="text"
                     name="verifyPassword"
-                    ref={register({ required: "Please verify your password"})}
+                    ref={register({ required: "Please verify your password", validate: value => value === enteredPassword || "Passwords must match"})}
                 />
 
                 {errors.verifyPassword && <p>{errors.verifyPassword.message}</p>}
