958be1fd4cb34663ec19487dc595ffe391267013
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-04-17 21:37:54 -0400

    removed unneeded import for clean up

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import {Link} from 'react-router-dom';
-import UserSignUp from './UserSignUp.js'
 import {useForm} from 'react-hook-form';
 
 function UserLogin() {
