2759127e4ab00ad1c03817a117b5c9e55859c45f
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-04-17 21:35:54 -0400

    Implemented react-hook-form for user login form
    added react-hook-form for validation to require input for email and password fields. also removed unneeded import for clean up

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,69 +1,52 @@
 import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 import UserSignUp from './UserSignUp.js'
-
+import {useForm} from 'react-hook-form';
 
 function UserLogin() {
 
-
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-        admin: false
-    });
-
-
-    //handle any changes made to input fields email and password
-    const handleChanges = event => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.value
-        })
-    }
-
-    //handle any changes made to the checkbox field
-    const handleCheckboxChanges = event => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.checked
-        });
-    }
+    //adjusted the 'mode' argument from the default value of 'onSubmit' for the useForm hook, in order to allow live error changes as user types
+    const { register, handleSubmit, errors } = useForm({mode: 'onChange'});
 
     //handle form submit
-    const submitForm = event => {
+    const submitForm = (data, event) => {
         event.preventDefault();
+
+        console.log("data: ", data)
+
         //add login action for redux 
     }
 
     return (
         <div>
-            <form onSubmit={submitForm}>
+            <form onSubmit={handleSubmit(submitForm)}>
 
-            <label for="email">Email: </label>
+               <label for="email">Email: </label>
                <input
                     id="email"
                     type="email"
                     name="email"
-                    onChange={handleChanges}
-                    value={user.email}
+                    ref={register({required: "Email is required to login"})}
                 />
+
+                {errors.email && <p>{errors.email.message}</p>}
 
                 <label for="password">Password: </label>
                 <input
                     id="password"
                     type="password"
                     name="password"
-                    onChange={handleChanges}
-                    value={user.password}
+                    ref={register({required: "Password is required to login"})}
                 />
+
+                {errors.password && <p>{errors.password.message}</p>}
 
                 <label for="admin">Are you an administrator?</label>
                 <input 
                     id="admin"
                     type="checkbox"
                     name="admin"
-                    onChange={handleCheckboxChanges}
-                    value={user.admin}
+                    ref={register}
                 />
 
                 <button type="submit">Log in</button>
