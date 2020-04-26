577ef83a1abd9ee9446d0bc1b867e270977ead8f
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-04-25 18:52:27 -0700

    Added Styling for Sidebar
    Added proper flexbox styling and padding for content inside sidebar

--- src/components/UserDashboard/UserSidebar/LeaderBoard/LeaderBoard.js
+++ src/components/UserDashboard/UserSidebar/LeaderBoard/LeaderBoard.js
@@ -5,13 +5,17 @@ class LeaderBoard extends React.Component{
     render(){
         return(
             <div className='leader-board'>
-                <h1 style={{fontSize: "25px"}}>Leaderboard</h1>
-                <>
+
+                <div className ='sidebar-title'>
+                    <h1>Leaderboard</h1>
+                </div>
+                
+                <div className='sidebar-content'>
                 <h2>Cybersecurity <br></br> 5000 Points</h2>
                 <h2>Expericeces <br></br> 4965 Points</h2>
                 <h2>Development <br></br> 4500 Points</h2>
                 <h2>Customer Support <br></br> 4350 Points</h2>
-                </>
+                </div>
             </div>
         )
     }
--- src/components/UserDashboard/UserSidebar/PersonalGoals/PersonalGoals.js
+++ src/components/UserDashboard/UserSidebar/PersonalGoals/PersonalGoals.js
@@ -8,13 +8,17 @@ class PersonalGoals extends React.Component{
     render(){
         return(
             <div className='personal-goals'>
-                <h1  style={{fontSize: "25px"}}>Personal Goals</h1>
-                <>
+
+                <div className ='sidebar-title'>
+                    <h1>Personal Goals</h1>
+                </div>
+                
+                <div className= 'sidebar-content'>
                 <h2>Drink {goals.water} glasses of water<br></br> {(goals.water - this.props.water) > 0 ? (goals.water - this.props.water) + " more to go!" : "Complete"}</h2>
                 <h2>Get {goals.sleep} hours of sleep <br></br> {(goals.sleep - this.props.sleep) > 0 ? (goals.sleep - this.props.sleep) + " more hours needed!" : "Complete"}</h2>
                 <h2>Exercise for {goals.exercise} minutes<br></br> {(goals.exercise - this.props.exercise) > 0 ? (goals.exercise - this.props.exercise) + " more minutes to go!" : "Complete"}</h2>
                 <h2>Take {goals.breaks} breaks<br></br>{(goals.breaks - this.props.breaks) > 0 ? (goals.breaks - this.props.breaks) + " more to go! " : "Complete"}</h2>
-                </>
+                </div>
             </div>
         )
     }
