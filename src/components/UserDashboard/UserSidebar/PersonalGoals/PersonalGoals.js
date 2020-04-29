4c62ca25465486ce4fdebea854fa12bbec5d8169
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-04-29 02:13:55 -0700

    Added Color to Personal Goals
    Added Color to Personal Goals so user can see at a glance what they need to work on

--- src/components/UserDashboard/UserSidebar/PersonalGoals/PersonalGoals.js
+++ src/components/UserDashboard/UserSidebar/PersonalGoals/PersonalGoals.js
@@ -14,25 +14,25 @@ class PersonalGoals extends React.Component{
                 </div>
                 
                 <div className= 'sidebar-content'>
-                
-                    <div className= 'sidebar-content-categrories'>
-                        <h3>Water</h3>
-                        <h4>{(goals.water - this.props.water) > 0 ? (goals.water - this.props.water) + " more glasses!" : "Complete"}</h4>
-                    </div>
                     
                     <div className= 'sidebar-content-categrories'>
+                        <h3>Water</h3>
+                        {(goals.water - this.props.water) > 0 ? <h4 className='incomplete'>{(goals.water - this.props.water)} more glasses!</h4>: <h4 className='complete'>Complete</h4>}
+                    </div>
+
+                    <div className= 'sidebar-content-categrories'>
                         <h3>Exercise</h3>
-                        <h4>{(goals.exercise - this.props.exercise) > 0 ? (goals.exercise - this.props.exercise) + " more minutes" : "Complete"}</h4>
+                        {(goals.exercise - this.props.exercise) > 0 ? <h4 className= 'incomplete'>{(goals.exercise - this.props.exercise)} more minutes!</h4> : <h4 className='complete'>Complete</h4>}
                     </div>
 
                     <div className= 'sidebar-content-categrories'>
                         <h3>Sleep</h3>
-                        <h4>{(goals.sleep - this.props.sleep) > 0 ? (goals.sleep - this.props.sleep) + " more hours!" : "Complete"}</h4>
+                        {(goals.sleep - this.props.sleep) > 0 ? <h4 className= 'incomplete'>{(goals.sleep - this.props.sleep)} more hours!</h4>: <h4 className='complete'>Complete</h4>}
                     </div>
 
                     <div className= 'sidebar-content-categrories'>
                         <h3>Breaks</h3>
-                        <h4>{(goals.breaks - this.props.breaks) > 0 ? (goals.breaks - this.props.breaks) + " more to go! " : "Complete"}</h4>
+                        {(goals.breaks - this.props.breaks) > 0 ? <h4 className= 'incomplete'>Take {(goals.breaks - this.props.breaks)} more!</h4>: <h4 className='complete'>Complete</h4>}
                     </div>
                 
                 </div>
