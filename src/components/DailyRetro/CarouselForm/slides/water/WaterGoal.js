c827993b6912738f5d820cfb2b1c0964bb4a0275
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-04-30 15:12:06 -0400

    continuing setting up points goals, needing to merge other code to continue working

--- src/components/DailyRetro/CarouselForm/slides/water/WaterGoal.js
+++ src/components/DailyRetro/CarouselForm/slides/water/WaterGoal.js
@@ -1,11 +1,25 @@
 import React, {useState} from "react";
 import {connect} from 'react-redux';
 
-function WaterGoal() {
+function WaterGoal(props) {
+
+    function setAddedPoints() {
+        if (props.water * props.pointsPerMetric.water.points > props.pointsPerMetric.water.max) {
+            return <h2>+ {props.pointsPerMetric.water.max} </h2>
+        }
+        else {
+            return <h2> + {props.water * props.pointsPerMetric.water.points} </h2>
+        }
+    }
+
 
     return(
         <div className="metricFormGoal">
-
+            <div className= "formGoalHeader">
+                <h2>{props.water} glasses of water</h2>
+                {setAddedPoints()}
+                {/* {(props.water * props.pointsPerMetric.water.points > props.pointsPerMetric.water.max ? <h2>+{props.pointsPerMetric.water.max}</h2> : props.pointsPerMetric.water.points * props.water)} */}
+            </div>
         </div>
     );
 }
