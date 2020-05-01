fb62f7f1a72344adeaf12a21f506ce0405ab5529
alexis villaraza <alexis.borja@gmail.com>
2020-04-30 17:25:09 -0700

    fixed capitals in naming

--- src/components/DailyRetro/Form/Form.js
+++ src/components/DailyRetro/Form/Form.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import WaterSlide from '../CarouselForm/slides/Water/WaterSlide.js'
-import ExerciseSlide from '../CarouselForm/slides/Exercise/ExerciseSlide.js'
-import SleepSlide from '../CarouselForm/slides/Sleep/SleepSlide.js'
-import BreaksSlide from '../CarouselForm/slides/Breaks/BreaksSlide.js'
+import React from "react";
+import WaterSlide from "../CarouselForm/slides/Water/WaterSlide.js";
+import ExerciseSlide from "../CarouselForm/slides/Exercise/ExerciseSlide.js";
+import SleepSlide from "../CarouselForm/slides/Sleep/SleepSlide.js";
+import BreaksSlide from "../CarouselForm/slides/Breaks/BreaksSlide.js";
 
-class Form extends React.Component{
+class Form extends React.Component {
   render() {
     return (
       <div>
-
         {/* TODO: Put these inside a carousel */}
 
         <WaterSlide />
@@ -16,7 +15,7 @@ class Form extends React.Component{
         <SleepSlide />
         <BreaksSlide />
       </div>
-    )
+    );
   }
 }
 
