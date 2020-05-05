97330d12eed6c42e80132f5369d997fc6fc53895
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-05-05 14:34:56 -0400

    passed down next/previous functions as props to the BreaksQuestion, and added arrow buttons to the BreaksQuestion component that call said functions on the onClick

--- src/components/DailyRetro/CarouselForm/slides/Breaks/BreaksQuestion.js
+++ src/components/DailyRetro/CarouselForm/slides/Breaks/BreaksQuestion.js
@@ -5,7 +5,9 @@ class BreaksQuestion extends React.Component{
   render() {
     return (
       <div>
+        <button onClick={this.props.previous}>{"<---------"}</button>
         <h3>How many breaks did you take today?</h3>
+        <button onClick={this.props.next}>{"--------->"}</button>
       </div>
     )
   }
