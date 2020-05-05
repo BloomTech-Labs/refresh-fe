5cba1ed70a4c468fd9474cb2c01e49d861569bb5
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-05-05 14:24:52 -0400

    passed down next/previous functions as props to the SleepQuestion, and added arrow buttons to the SleepQuestion component that call said functions on the onClick

--- src/components/DailyRetro/CarouselForm/slides/Sleep/SleepSlide.js
+++ src/components/DailyRetro/CarouselForm/slides/Sleep/SleepSlide.js
@@ -10,7 +10,7 @@ class SleepSlide extends React.Component {
   render() {
     return (
       <div>
-        <SleepQuestion />
+        <SleepQuestion next={this.props.next} previous={this.props.previous}/>
 
         <div>
           <SleepCard />
