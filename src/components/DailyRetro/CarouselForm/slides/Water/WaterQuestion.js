36ce1222263174169b848466e40ea10ecf25339d
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-05-06 13:05:12 -0700

    Added button to Dashboard
    Replaced Back button in Slide navigation with a button to User Dashboard

--- src/components/DailyRetro/CarouselForm/slides/Water/WaterQuestion.js
+++ src/components/DailyRetro/CarouselForm/slides/Water/WaterQuestion.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 class WaterQuestion extends React.Component{
   render() {
     return (
       <div className='slide-nav'>
+        <Link to="/UserDashboard">
         <button onClick={this.props.previous}>{"<----DASHBOARD"}</button>
+        </Link>
         <h2>How much water did you drink?</h2>
         <button onClick={this.props.next}>{"NEXT---->"}</button>
       </div>
