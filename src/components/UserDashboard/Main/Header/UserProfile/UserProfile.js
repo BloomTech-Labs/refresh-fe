7f2e6677cf4d0674a6b48f04cfa1cfc85f6f486a
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-05-21 12:16:55 -0400

    added useravatar svg to userProfile component for user side, and added ternary to show that svg if user has not yet uploaded their own image

--- src/components/UserDashboard/Main/Header/UserProfile/UserProfile.js
+++ src/components/UserDashboard/Main/Header/UserProfile/UserProfile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import profilepic from './ProfilePic.svg'
+import UserAvatar from '../../../../../views/private/user-list/useravatar.svg'
 import {connect} from 'react-redux';
 
 
@@ -12,7 +13,9 @@ class UserProfile extends React.Component{
     render(){
         return(
             <div className='user-profile'>
-                <img className='profile-pic' src={`https://lab23-refresh-be.herokuapp.com/${this.props.avatar}`} alt="profile-pic" width="70px"></img>
+                
+                {(this.props.avatar ? <img className='profile-pic' src={`https://lab23-refresh-be.herokuapp.com/${this.props.avatar}`} width="70px"></img> : <img className='profile-pic' src={UserAvatar} width="70px"></img>)}
+                
                 <UserInfo />
                 <Dropdown />
 
